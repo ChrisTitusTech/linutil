075927204dd019cdefca54b0b3ccde157765945e
Chris Titus <contact@christitus.com>
2024-06-28 21:12:23 -0500

    start fix

--- start.sh
+++ start.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
-curl https://github.com/ChrisTitusTech/linutil/raw/main/linutil -o linutil
+
+RC='\e[0m'
+RED='\e[31m'
+YELLOW='\e[33m'
+GREEN='\e[32m'
+
+# Check if the home directory and linuxtoolbox folder exist, create them if they don't
+LINUXTOOLBOXDIR="$HOME/linuxtoolbox"
+
+if [[ ! -d "$LINUXTOOLBOXDIR" ]]; then
+    echo -e "${YELLOW}Creating linuxtoolbox directory: $LINUXTOOLBOXDIR${RC}"
+    mkdir -p "$LINUXTOOLBOXDIR"
+    echo -e "${GREEN}linuxtoolbox directory created: $LINUXTOOLBOXDIR${RC}"
+fi
+
+cd "$LINUXTOOLBOXDIR" || exit
+
+curl -L https://github.com/ChrisTitusTech/linutil/raw/main/linutil -o linutil
 chmod +x ./linutil
 ./linutil
