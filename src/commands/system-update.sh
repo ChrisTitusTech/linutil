6e3392a8628aa282ef9e4594833280d3af5bc83b
AlbydS <119180144+AlbydST@users.noreply.github.com>
2024-07-16 13:36:44 +0000

    Many improvements to System Update script
    I added support for:
    
    - Gentoo (Portage)
    - Void (XBPS)
    - NixOS (Nix)
    - Slackware (Slackpkg)
    - Alpine (APK)
    
    I also modified the script to check which privilege escalation software is available and use that for commands that require running as root. Currently supports both sudo and doas, and fallbacks to su -c if neither is present.

--- src/commands/system-update.sh
+++ src/commands/system-update.sh
@@ -19,7 +19,7 @@ checkEnv() {
         fi
     done
     ## Check Package Handler
-    PACKAGEMANAGER='apt-get nala dnf pacman zypper yum'
+    PACKAGEMANAGER='apt-get nala dnf pacman zypper yum emerge xbps-install nix-env slackpkg apk'
     for pgm in ${PACKAGEMANAGER}; do
         if command_exists ${pgm}; then
             PACKAGER=${pgm}
@@ -32,6 +32,16 @@ checkEnv() {
         printf "${RED}Can't find a supported package manager${RC}\n"
         exit 1
     fi
+
+    if command_exists sudo; then
+        SUDO_CMD="sudo"
+    elif command_exists doas && [ -f "/etc/doas.conf" ]; then
+        SUDO_CMD="doas"
+    else
+        SUDO_CMD="su -c"
+    fi
+
+    echo "Using $SUDO_CMD as privilege escalation software"
 
     ## Check SuperUser Group
     SUPERUSERGROUP='wheel sudo root'
@@ -62,11 +72,11 @@ fastUpdate() {
         pacman)
             if ! command_exists yay && ! command_exists paru; then
                 printf "Installing yay as AUR helper...\n"
-                sudo ${PACKAGER} --noconfirm -S base-devel || { printf "${RED}Failed to install base-devel${RC}\n"; exit 1; }
-                cd /opt && sudo git clone https://aur.archlinux.org/yay-git.git && sudo chown -R ${USER}:${USER} ./yay-git
+                ${SUDO_CMD} ${PACKAGER} --noconfirm -S base-devel || { printf "${RED}Failed to install base-devel${RC}\n"; exit 1; }
+                cd /opt && ${SUDO_CMD} git clone https://aur.archlinux.org/yay-git.git && ${SUDO_CMD} chown -R ${USER}:${USER} ./yay-git
                 cd yay-git && makepkg --noconfirm -si || { printf "${RED}Failed to install yay${RC}\n"; exit 1; }
             else
-                printf "Aur helper already installed\n"
+                printf "AUR helper already installed\n"
             fi
             if command_exists yay; then
                 AUR_HELPER="yay"
@@ -77,37 +87,59 @@ fastUpdate() {
                 exit 1
             fi
             ${AUR_HELPER} --noconfirm -S rate-mirrors-bin
-            sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+            ${SUDO_CMD} cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
             
             # If for some reason DTYPE is still unknown use always arch so the rate-mirrors does not fail
             dtype_local=${DTYPE}
             if [ "${DTYPE}" = "unknown" ]; then
                 dtype_local="arch"
             fi
-            sudo rate-mirrors --top-mirrors-number-to-retest=5 --disable-comments --save /etc/pacman.d/mirrorlist --allow-root ${dtype_local}
+            ${SUDO_CMD} rate-mirrors --top-mirrors-number-to-retest=5 --disable-comments --save /etc/pacman.d/mirrorlist --allow-root ${dtype_local}
             ;;
         apt-get|nala)
-            sudo apt-get update
+            ${SUDO_CMD} apt-get update
             if ! command_exists nala; then
-                sudo apt-get install -y nala || { printf "${YELLOW}Falling back to apt-get${RC}\n"; PACKAGER="apt-get"; }
+                ${SUDO_CMD} apt-get install -y nala || { printf "${YELLOW}Falling back to apt-get${RC}\n"; PACKAGER="apt-get"; }
             fi
             if [ "${PACKAGER}" = "nala" ]; then
-                sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
-                sudo nala update
+                ${SUDO_CMD} cp /etc/apt/sources.list /etc/apt/sources.list.bak
+                ${SUDO_CMD} nala update
                 PACKAGER="nala"
             fi
-            sudo ${PACKAGER} upgrade -y
+            ${SUDO_CMD} ${PACKAGER} full-upgrade -y
             ;;
         dnf)
-            sudo ${PACKAGER} update -y
+            ${SUDO_CMD} ${PACKAGER} update -y
             ;;
         zypper)
-            sudo ${PACKAGER} refresh
-            sudo ${PACKAGER} update -y
+            ${SUDO_CMD} ${PACKAGER} refresh
+            ${SUDO_CMD} ${PACKAGER} update -y
             ;;
         yum)
-            sudo ${PACKAGER} update -y
-            sudo ${PACKAGER} upgrade -y
+            ${SUDO_CMD} ${PACKAGER} update -y
+            ${SUDO_CMD} ${PACKAGER} upgrade -y
+            ;;
+        emerge)
+            ${SUDO_CMD} ${PACKAGER}-webrsync
+            ${SUDO_CMD} ${PACKAGER} --sync
+            ${SUDO_CMD} ${PACKAGER} -vuDN @world
+            ;;
+        xbps-install)
+            ${SUDO_CMD} ${PACKAGER} -Syu --yes
+            ;;
+        nix-env)
+            ${SUDO_CMD} nix-channel --update
+            ${SUDO_CMD} nixos-rebuild switch
+            ${SUDO_CMD} ${PACKAGER} -u '*'
+            ;;
+        slackpkg)
+            ${SUDO_CMD} ${PACKAGER} update
+            ${SUDO_CMD} ${PACKAGER} install-new
+            ${SUDO_CMD} ${PACKAGER} upgrade-all
+            ;;
+        apk)
+            ${SUDO_CMD} ${PACKAGER} update
+            ${SUDO_CMD} ${PACKAGER} upgrade --available --no-cache --no-progress
             ;;
         *)
             printf "${RED}Unsupported package manager: ${PACKAGER}${RC}\n"
@@ -120,19 +152,41 @@ updateSystem() {
     printf "${GREEN}Updating system${RC}\n"
     case ${PACKAGER} in
         nala|apt-get)
-            sudo ${PACKAGER} update -y
-            sudo ${PACKAGER} upgrade -y
+            ${SUDO_CMD} ${PACKAGER} update -y
+            ${SUDO_CMD} ${PACKAGER} full-upgrade -y
             ;;
         yum|dnf)
-            sudo ${PACKAGER} update -y
-            sudo ${PACKAGER} upgrade -y
+            ${SUDO_CMD} ${PACKAGER} update -y
+            ${SUDO_CMD} ${PACKAGER} upgrade -y
             ;;
         pacman)
-            sudo ${PACKAGER} -Syu --noconfirm
+            ${SUDO_CMD} ${PACKAGER} -Syu --noconfirm
             ;;
         zypper)
-            sudo ${PACKAGER} refresh
-            sudo ${PACKAGER} update -y
+            ${SUDO_CMD} ${PACKAGER} refresh
+            ${SUDO_CMD} ${PACKAGER} update -y
+            ;;
+        emerge)
+            ${SUDO_CMD} ${PACKAGER}-webrsync
+            ${SUDO_CMD} ${PACKAGER} --sync -v
+            ${SUDO_CMD} ${PACKAGER} -vuDN @world
+            ;;
+        xbps-install)
+            ${SUDO_CMD} ${PACKAGER} -Syu --yes
+            ;;
+        nix-env)
+            ${SUDO_CMD} nix-channel --update
+            ${SUDO_CMD} nixos-rebuild switch
+            ${SUDO_CMD} ${PACKAGER} -u '*'
+            ;;
+        slackpkg)
+            ${SUDO_CMD} ${PACKAGER} update
+            ${SUDO_CMD} ${PACKAGER} install-new
+            ${SUDO_CMD} ${PACKAGER} upgrade-all
+            ;;
+        apk)
+            ${SUDO_CMD} ${PACKAGER} update
+            ${SUDO_CMD} ${PACKAGER} upgrade --available --no-cache
             ;;
         *)
             printf "${RED}Unsupported package manager: ${PACKAGER}${RC}\n"
