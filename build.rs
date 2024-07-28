89c942fd87215965a41e10d826d2ca284d718387
Liam <33645555+lj3954@users.noreply.github.com>
2024-07-28 00:47:39 -0700

    refactor: Improvements to logic and readability of build script

--- build.rs
+++ build.rs
@@ -1,12 +1,15 @@
 use std::{
     env, fs,
-    io::Write,
+    io::{Read, Write},
     path::{Path, PathBuf},
 };
 
 const SCRIPT_PATH: &str = "src/commands/";
 
 fn main() {
+    // Rerun build step if the build script is modified
+    println!("cargo:rerun-if-changed=build.rs");
+
     let out_dir = env::var("OUT_DIR").unwrap();
     let file_list = get_script_list(Path::new(SCRIPT_PATH));
     replace_source(file_list, out_dir.into());
@@ -23,7 +26,7 @@ fn get_script_list(path: &Path) -> Vec<PathBuf> {
             if entry.file_type().map_or(false, |f| f.is_dir()) {
                 get_script_list(&path).into()
             } else {
-                let is_script = fs::read_to_string(&path).map_or(false, |f| f.starts_with("#!"));
+                let is_script = has_shell_ext(&path) && starts_with_shebang(&path);
                 is_script.then_some(vec![path])
             }
         })
@@ -33,6 +36,7 @@ fn get_script_list(path: &Path) -> Vec<PathBuf> {
 
 fn replace_source(files: Vec<PathBuf>, out_dir: PathBuf) {
     for file in files {
+        // Rerun build step if any script is modified
         println!("cargo:rerun-if-changed={}", file.display());
 
         let mut out_file = create_out_file(&file, out_dir.clone());
@@ -58,8 +62,19 @@ fn replace_source(files: Vec<PathBuf>, out_dir: PathBuf) {
 }
 
 fn create_out_file(file: &Path, out_dir: PathBuf) -> fs::File {
-    let out_file = out_dir.clone().join(&file);
+    let out_file = out_dir.clone().join(file);
     let out_file_parent = out_file.parent().unwrap();
     std::fs::create_dir_all(out_file_parent).unwrap();
     fs::File::create(out_file).unwrap()
+}
+
+fn has_shell_ext(file: &Path) -> bool {
+    file.extension().map_or(true, |ext| ext == "sh")
+}
+
+fn starts_with_shebang(file: &Path) -> bool {
+    fs::File::open(file).map_or(false, |mut file| {
+        let mut two_byte_buffer = [0; 2];
+        file.read_exact(&mut two_byte_buffer).is_ok() && two_byte_buffer == *b"#!"
+    })
 }
