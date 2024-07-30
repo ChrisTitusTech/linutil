f1f3937e49fa36e73f4caff91bcb03a2632d74b8
infstate <altcoding127@gmail.com>
2024-07-29 20:23:02 -0700

    Remove check and add comments

--- src/commands/dotfiles/dwmtitus-setup.sh
+++ src/commands/dotfiles/dwmtitus-setup.sh
@@ -2,22 +2,19 @@
 
 setupDWM() {
     echo "Installing DWM-Titus if not already installed"
-    if ! command_exists dwm; then
-        case "$PACKAGER" in
-            pacman)
-                sudo "$PACKAGER" -S --noconfirm --needed base-devel libx11 libxinerama libxft imlib2
-                ;;
-            *)
-                sudo "$PACKAGER" install -y build-essential libx11-dev libxinerama-dev libxft-dev libimblib2-dev
-                ;;
-        esac
-    else
-        echo "DWM is already installed."
-    fi
-   cd $HOME && git clone https://github.com/ChrisTitusTech/dwm-titus.git
-   cd dwm-titus/
-   sudo ./setup.sh
-   sudo make clean install
+    case "$PACKAGER" in # Install pre-Requisites
+        pacman)
+            sudo "$PACKAGER" -S --noconfirm --needed base-devel libx11 libxinerama libxft imlib2
+            ;;
+        *)
+            sudo "$PACKAGER" install -y build-essential libx11-dev libxinerama-dev libxft-dev libimblib2-dev
+            ;;
+    esac
+   cd $HOME && git clone https://github.com/ChrisTitusTech/dwm-titus.git # CD to Home directory to install dwm-titus
+   # This path can be changed (e.g. to linux-toolbox directory)
+   cd dwm-titus/ # Hardcoded path, maybe not the best.
+   sudo ./setup.sh # Run setup
+   sudo make clean install # Run make clean install
 }
 
 checkEnv
