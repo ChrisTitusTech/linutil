16c6363a95f0ad3fbc4017ee59d4e41a9a112f89
Soapydev <alexandre.beauchamp.thibault@gmail.com>
2024-08-15 18:13:47 -0400

    Update theme to latest version

--- src/state.rs
+++ src/state.rs
@@ -69,7 +69,7 @@ impl AppState {
     pub fn draw(&mut self, frame: &mut Frame) {
         let longest_tab_display_len = TABS
             .iter()
-            .map(|tab| tab.name.len() + self.theme.tab_icon.len())
+            .map(|tab| tab.name.len() + self.theme.tab_icon().len())
             .max()
             .unwrap_or(0);
 
@@ -88,15 +88,15 @@ impl AppState {
         let tabs = TABS.iter().map(|tab| tab.name).collect::<Vec<_>>();
 
         let tab_hl_style = if let Focus::TabList = self.focus {
-            Style::default().reversed().fg(self.theme.tab_color)
+            Style::default().reversed().fg(self.theme.tab_color())
         } else {
-            Style::new().fg(self.theme.tab_color)
+            Style::new().fg(self.theme.tab_color())
         };
 
         let list = List::new(tabs)
             .block(Block::default().borders(Borders::ALL))
             .highlight_style(tab_hl_style)
-            .highlight_symbol(self.theme.tab_icon);
+            .highlight_symbol(self.theme.tab_icon());
         frame.render_stateful_widget(list, left_chunks[1], &mut self.current_tab);
 
         let chunks = Layout::default()
@@ -122,7 +122,7 @@ impl AppState {
         let mut items: Vec<Line> = Vec::new();
         if !self.at_root() {
             items.push(
-                Line::from(format!("{}  ..", self.theme.dir_icon)).style(self.theme.dir_color),
+                Line::from(format!("{}  ..", self.theme.dir_icon())).style(self.theme.dir_color()),
             );
         }
 
@@ -131,11 +131,11 @@ impl AppState {
                  node, has_children, ..
              }| {
                 if *has_children {
-                    Line::from(format!("{}  {}", self.theme.dir_icon, node.name))
-                        .style(self.theme.dir_color)
+                    Line::from(format!("{}  {}", self.theme.dir_icon(), node.name))
+                        .style(self.theme.dir_color())
                 } else {
-                    Line::from(format!("{}  {}", self.theme.cmd_icon, node.name))
-                        .style(self.theme.cmd_color)
+                    Line::from(format!("{}  {}", self.theme.cmd_icon(), node.name))
+                        .style(self.theme.cmd_color())
                 }
             },
         ));
