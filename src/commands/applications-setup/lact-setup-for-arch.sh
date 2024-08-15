c408e1a58b8fb41e862abc8a25a1c3b1045cecd8
gunther6000 <gunther6000@gmail.com>
2024-08-15 18:30:41 -0400

    Update lact-setup-for-arch.sh

--- src/commands/applications-setup/lact-setup-for-arch.sh
+++ src/commands/applications-setup/lact-setup-for-arch.sh
@@ -19,7 +19,7 @@ setupLACT() {
             pacman)
                 if ! command_exists paru && ! command_exists yay; then
                     echo "Neither paru nor yay is installed. Installing recommended AUR helper paru..."
-                    sudo ${PACKAGER} --noconfirm --needed git base-devel
+                    sudo ${PACKAGER} -S --noconfirm --needed git base-devel
                     git clone https://aur.archlinux.org/paru.git
                     cd paru
                     makepkg -si
