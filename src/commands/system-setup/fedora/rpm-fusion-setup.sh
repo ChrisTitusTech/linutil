d89d1e24665aa1c6a4f56246df70847867d35c2f
stormseal <178997548+stormseal@users.noreply.github.com>
2024-08-23 18:37:04 +0200

    Update rpm-fusion-setup.sh
    Removed useless brackets

--- src/commands/system-setup/fedora/rpm-fusion-setup.sh
+++ src/commands/system-setup/fedora/rpm-fusion-setup.sh
@@ -7,7 +7,7 @@
 installRPMFusion() {
     case $PACKAGER in
         dnf)
-            if [[ ! -e "/etc/yum.repos.d/rpmfusion-free.repo" ]]; then
+            if [ ! -e "/etc/yum.repos.d/rpmfusion-free.repo" ]; then
                 echo "Installing RPM Fusion..."
                 sudo "$PACKAGER" install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
                 sudo "$PACKAGER" config-manager --enable fedora-cisco-openh264
