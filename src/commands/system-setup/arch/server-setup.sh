95ba3ec838dbfab0dfc5939930bee8905137e0fc
sevu11 <johan.sorman@icloud.com>
2024-08-23 16:49:49 +0200

    Update server-setup.sh
    Adding SE keymap.

--- src/commands/system-setup/arch/server-setup.sh
+++ src/commands/system-setup/arch/server-setup.sh
@@ -182,7 +182,7 @@ keymap () {
     echo -ne "
     Please select key board layout from this list"
     # These are default key maps as presented in official arch repo archinstall
-    options=(us by ca cf cz de dk es et fa fi fr gr hu il it lt lv mk nl no pl ro ru sg ua uk)
+    options=(us by ca cf cz de dk es et fa fi fr gr hu il it lt lv mk nl no pl ro ru se sg ua uk)
 
     select_option "${options[@]}"
     keymap=${options[$?]}
