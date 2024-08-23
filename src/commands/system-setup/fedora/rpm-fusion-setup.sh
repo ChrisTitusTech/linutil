81b53da48c361c98a486e6dda5bc3474661f93d1
stormseal <178997548+stormseal@users.noreply.github.com>
2024-08-23 13:52:26 +0200

    Added RPM Fusion installation script
    Script to install RPM Fusion on Fedora

---
+++ src/commands/system-setup/fedora/rpm-fusion-setup.sh
@@ -0,0 +1,26 @@
+#!/bin/sh -e
+
+. "$(dirname "$0")/../../common-script.sh"
+
+# https://rpmfusion.org/Configuration
+
+installRPMFusion() {
+    case $PACKAGER in
+        dnf)
+            if [[ ! -e "/etc/yum.repos.d/rpmfusion-free.repo" ]]; then
+                echo "Installing RPM Fusion..."
+                sudo "$PACKAGER" install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+                sudo "$PACKAGER" config-manager --enable fedora-cisco-openh264
+                echo "RPM Fusion installed"
+            else
+                echo "RPM Fusion already installed"
+            fi
+            ;;
+        *)
+            echo "Unsupported distribution: $DTYPE"
+            ;;
+    esac
+}
+
+checkEnv
+installRPMFusion
