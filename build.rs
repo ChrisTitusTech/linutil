664efcde9d1ff23db8c943f9884a3ec744dba2cb
Liam <33645555+lj3954@users.noreply.github.com>
2024-09-02 13:55:40 -0700

    feat: Display build date to user, rather than current date

--- build.rs
+++ build.rs
@@ -1,4 +1,9 @@
 fn main() {
     // Rebuild program if any file in commands directory changes.
     println!("cargo:rerun-if-changed=src/commands");
+    // Add current date as a variable to be displayed in the 'Linux Toolbox' text.
+    println!(
+        "cargo:rustc-env=BUILD_DATE={}",
+        chrono::Local::now().format("%Y-%m-%d")
+    );
 }
