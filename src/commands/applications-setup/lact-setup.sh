c80378d17d927a9b307af7902c9915c381f74d8a
Nightmare1984 <gunther6000@gmail.com>
2024-09-03 02:11:13 -0400

    Removed lines 14 -20

--- src/commands/applications-setup/lact-setup.sh
+++ src/commands/applications-setup/lact-setup.sh
@@ -4,22 +4,12 @@
 
 setuplact() {
     echo "Installing Lact"
-
     if command_exists lact; then
         echo "Lact is already installed."
         return
     fi
-
-    checkAURHelper  # Ensure we have an AUR helper available
-
-    if [ -z "$AUR_HELPER" ]; then
-        echo -e "${RED}No suitable AUR helper found to install Lact!${RC}"
-        exit 1
-    fi
-
-    echo "Using AUR helper: ${AUR_HELPER}"
+    checkAURHelper
     $AUR_HELPER -S --noconfirm lact
-
     if command_exists systemctl; then
        $ESCALATION_TOOL systemctl enable --now lactd
     fi
