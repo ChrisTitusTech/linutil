9f48e2466f58d53f925ce8dbe41f53c3e856effc
Andrii Dokhniak <adokhniak@gmail.com>
2024-09-05 13:10:53 +0200

    Cargo will rebuild if anything changes in src/, recursively. E.g src/commands/ is also checked

--- build.rs
+++ build.rs
@@ -1,9 +1,4 @@
 fn main() {
-    // Rebuild program if any file in commands directory changes.
-    println!("cargo:rerun-if-changed=src/commands");
-    // Rerun build script if any code is modified
-    println!("cargo:rerun-if-changed=src");
-
     // Add current date as a variable to be displayed in the 'Linux Toolbox' text.
     println!(
         "cargo:rustc-env=BUILD_DATE={}",
