3c29ab2709423b21cc44de133be58904efe47668
Nightmare1984 <gunther6000@gmail.com>
2024-09-06 03:43:21 -0400

    I Added Other Distros and Moved The CheckAURHelper

--- src/commands/applications-setup/lact-setup.sh
+++ src/commands/applications-setup/lact-setup.sh
@@ -5,17 +5,45 @@
 setuplact() {
     echo "Installing Lact"
     if command_exists lact; then
-        echo "Lact is already installed."
-        return
-    fi
-    checkAURHelper
-    $AUR_HELPER -S --noconfirm lact
-    if command_exists systemctl; then
-       $ESCALATION_TOOL systemctl enable --now lactd
-    fi
-
+        echo "Lact already Installed"
+    fi    
+        case $PACKAGER in
+        pacman)   
+            $AUR_HELPER -S --noconfirm lact
+            $ESCALATION_TOOL systemctl enable --now lactd
+            ;;
+        dnf)
+            VERSION=$(curl -s https://api.github.com/repos/ilya-zlobintsev/LACT/releases/latest | grep -Po '"tag_name": "\K[^"]*')
+            RPM_FILE="lact-${VERSION}-0.x86_64.fedora-40.rpm"
+            curl -L -o "$RPM_FILE" "https://github.com/ilya-zlobintsev/LACT/releases/latest/download/$RPM_FILE"
+            $ESCALATION_TOOL $PACKAGER install -y "$RPM_FILE"
+            $ESCALATION_TOOL systemctl enable --now lactd
+            rm "$RPM_FILE"
+            ;;
+        zypper)
+            VERSION=$(curl -s https://api.github.com/repos/ilya-zlobintsev/LACT/releases/latest | grep -Po '"tag_name": "\K[^"]*')
+            RPM_FILE="lact-${VERSION}-0.x86_64.opensuse-tumbleweed.rpm"
+            curl -L -o "$RPM_FILE" "https://github.com/ilya-zlobintsev/LACT/releases/latest/download/$RPM_FILE"
+            $ESCALATION_TOOL $PACKAGER in -y "$RPM_FILE"
+            $ESCALATION_TOOL systemctl enable --now lactd
+            rm "$RPM_FILE"
+            ;;
+        apt-get)
+            VERSION=$(curl -s https://api.github.com/repos/ilya-zlobintsev/LACT/releases/latest | grep -Po '"tag_name": "\K[^"]*')
+            DEB_FILE="lact_${VERSION}_amd64.deb"
+            curl -L -o "$DEB_FILE" "https://github.com/ilya-zlobintsev/LACT/releases/latest/download/$DEB_FILE"
+            $ESCALATION_TOOL $PACKAGER install -y "$DEB_FILE"
+            $ESCALATION_TOOL systemctl enable --now lactd
+            rm "$DEB_FILE"
+            ;;
+        *)
+            echo "Unsupported package manager: $PACKAGER"
+            exit 1
+            ;;
+        esac
 }
 
 checkEnv
+checkAURHelper
 checkEscalationTool
 setuplact
