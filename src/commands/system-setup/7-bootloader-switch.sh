29ce9d4d233b113d3fe0596314c359f02137d5d1
fam007e <faisalmoshiur@gmail.com>
2024-09-11 04:48:07 +0600

    dir call for common-script was erroneous which was fixed and posix compliant methods like printf are used so colored text could be used which previously showed just numbers i.e. ANSI escape codes.

--- src/commands/system-setup/7-bootloader-switch.sh
+++ src/commands/system-setup/7-bootloader-switch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
 # Load common functions and environment checks from common-script.sh
-. ./common-script.sh
+. ../common-script.sh
 
 BACKUP_DIR="/boot/backup/bootloader"
 TIMESTAMP=$(date +%Y%m%d-%H%M%S)
@@ -16,61 +16,61 @@ create_backup_dir() {
 # Detect current bootloader
 detect_bootloader() {
     if [ -d /boot/grub ]; then
-        echo "GRUB"
+        printf "GRUB\n"
     elif [ -d /boot/loader ]; then
-        echo "systemd-boot"
+        printf "systemd-boot\n"
     else
-        echo "Unknown"
+        printf "Unknown\n"
     fi
 }
 
 # Backup GRUB configuration
 backup_grub() {
     create_backup_dir
-    echo "${YELLOW}Backing up GRUB configuration...${RC}"
+    printf "${YELLOW}Backing up GRUB configuration...${RC}\n"
     $ESCALATION_TOOL tar -czf "$BACKUP_DIR/grub-backup-$TIMESTAMP.tar.gz" /boot/grub /etc/default/grub /etc/grub.d
-    echo "${GREEN}GRUB configuration backed up to $BACKUP_DIR/grub-backup-$TIMESTAMP.tar.gz${RC}"
+    printf "${GREEN}GRUB configuration backed up to $BACKUP_DIR/grub-backup-$TIMESTAMP.tar.gz${RC}\n"
 }
 
 # Backup systemd-boot configuration
 backup_systemd_boot() {
     create_backup_dir
-    echo "${YELLOW}Backing up systemd-boot configuration...${RC}"
+    printf "${YELLOW}Backing up systemd-boot configuration...${RC}\n"
     $ESCALATION_TOOL tar -czf "$BACKUP_DIR/systemd-boot-backup-$TIMESTAMP.tar.gz" /boot/loader
-    echo "${GREEN}systemd-boot configuration backed up to $BACKUP_DIR/systemd-boot-backup-$TIMESTAMP.tar.gz${RC}"
+    printf "${GREEN}systemd-boot configuration backed up to $BACKUP_DIR/systemd-boot-backup-$TIMESTAMP.tar.gz${RC}\n"
 }
 
 # Restore GRUB configuration
 restore_grub() {
     if [ -z "$1" ]; then
-        echo "${RED}Please provide the backup file for GRUB restore.${RC}"
+        printf "${RED}Please provide the backup file for GRUB restore.${RC}\n"
         exit 1
     fi
 
-    echo "${YELLOW}Restoring GRUB configuration from $1...${RC}"
+    printf "${YELLOW}Restoring GRUB configuration from %s...${RC}\n" "$1"
     $ESCALATION_TOOL tar -xzf "$1" -C /
     grub-mkconfig -o /boot/grub/grub.cfg
-    echo "${GREEN}GRUB configuration restored.${RC}"
+    printf "${GREEN}GRUB configuration restored.${RC}\n"
 }
 
 # Restore systemd-boot configuration
 restore_systemd_boot() {
     if [ -z "$1" ]; then
-        echo "${RED}Please provide the backup file for systemd-boot restore.${RC}"
+        printf "${RED}Please provide the backup file for systemd-boot restore.${RC}\n"
         exit 1
     fi
 
-    echo "${YELLOW}Restoring systemd-boot configuration from $1...${RC}"
+    printf "${YELLOW}Restoring systemd-boot configuration from %s...${RC}\n" "$1"
     $ESCALATION_TOOL tar -xzf "$1" -C /
-    echo "${GREEN}systemd-boot configuration restored.${RC}"
+    printf "${GREEN}systemd-boot configuration restored.${RC}\n"
 }
 
 # Switch to GRUB bootloader
 switch_to_grub() {
     backup_systemd_boot  # Backup systemd-boot before switching
-    echo "${YELLOW}Switching to GRUB...${RC}"
+    printf "${YELLOW}Switching to GRUB...${RC}\n"
 
-    case $DTYPE in
+    case "$DTYPE" in
         debian | ubuntu)
             $ESCALATION_TOOL apt-get install -y grub-efi
             grub-install
@@ -87,7 +87,7 @@ switch_to_grub() {
             grub2-mkconfig -o /boot/grub2/grub.cfg
             ;;
         *)
-            echo "${RED}Unsupported distribution for GRUB installation.${RC}"
+            printf "${RED}Unsupported distribution for GRUB installation.${RC}\n"
             exit 1
             ;;
     esac
@@ -96,14 +96,14 @@ switch_to_grub() {
 # Switch to systemd-boot bootloader
 switch_to_systemd_boot() {
     backup_grub  # Backup GRUB before switching
-    echo "${YELLOW}Switching to systemd-boot...${RC}"
+    printf "${YELLOW}Switching to systemd-boot...${RC}\n"
 
-    case $DTYPE in
+    case "$DTYPE" in
         debian | ubuntu | arch | fedora)
             $ESCALATION_TOOL bootctl install
             ;;
         *)
-            echo "${RED}Unsupported distribution for systemd-boot installation.${RC}"
+            printf "${RED}Unsupported distribution for systemd-boot installation.${RC}\n"
             exit 1
             ;;
     esac
@@ -111,53 +111,53 @@ switch_to_systemd_boot() {
 
 # Display list of backups for user to choose
 display_backup_list() {
-    echo "${YELLOW}Available backups:${RC}"
-    ls -1 $BACKUP_DIR | grep -E 'grub-backup|systemd-boot-backup'
+    printf "${YELLOW}Available backups:${RC}\n"
+    ls -1 "$BACKUP_DIR" | grep -E 'grub-backup|systemd-boot-backup'
 }
 
 # Interactive menu for bootloader switching
 menu() {
     CURRENT_BOOTLOADER=$(detect_bootloader)
 
-    echo "${YELLOW}Current bootloader detected: $CURRENT_BOOTLOADER${RC}"
+    printf "${YELLOW}Current bootloader detected: %s${RC}\n" "$CURRENT_BOOTLOADER"
     if [ "$CURRENT_BOOTLOADER" = "GRUB" ]; then
-        echo "${YELLOW}1) Switch to systemd-boot${RC}"
+        printf "${YELLOW}1) Switch to systemd-boot${RC}\n"
     elif [ "$CURRENT_BOOTLOADER" = "systemd-boot" ]; then
-        echo "${YELLOW}1) Switch to GRUB${RC}"
+        printf "${YELLOW}1) Switch to GRUB${RC}\n"
     fi
-    echo "${YELLOW}2) Restore backup${RC}"
-    echo "${YELLOW}3) Exit${RC}"
-    echo -n "Choose an option: "
-    read -r option
+    printf "${YELLOW}2) Restore backup${RC}\n"
+    printf "${YELLOW}3) Exit${RC}\n"
+    printf "Choose an option: "
+    read option
 
-    case $option in
+    case "$option" in
         1)
             if [ "$CURRENT_BOOTLOADER" = "GRUB" ]; then
                 switch_to_systemd_boot
             elif [ "$CURRENT_BOOTLOADER" = "systemd-boot" ]; then
                 switch_to_grub
             else
-                echo "${RED}Unknown bootloader detected. Cannot switch.${RC}"
+                printf "${RED}Unknown bootloader detected. Cannot switch.${RC}\n"
             fi
             ;;
         2)
             display_backup_list
-            echo -n "${YELLOW}Enter the name of the backup file to restore: ${RC}"
-            read -r backup_file
+            printf "${YELLOW}Enter the name of the backup file to restore: ${RC}"
+            read backup_file
             if echo "$backup_file" | grep -q "grub-backup"; then
                 restore_grub "$BACKUP_DIR/$backup_file"
             elif echo "$backup_file" | grep -q "systemd-boot-backup"; then
                 restore_systemd_boot "$BACKUP_DIR/$backup_file"
             else
-                echo "${RED}Invalid backup file selected.${RC}"
+                printf "${RED}Invalid backup file selected.${RC}\n"
             fi
             ;;
         3)
-            echo "${GREEN}Exiting.${RC}"
+            printf "${GREEN}Exiting.${RC}\n"
             exit 0
             ;;
         *)
-            echo "${RED}Invalid option selected.${RC}"
+            printf "${RED}Invalid option selected.${RC}\n"
             exit 1
             ;;
     esac
