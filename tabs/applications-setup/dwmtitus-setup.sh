81899a4799b6a34c17cc90ce43a42654d89516f8
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-09-13 00:58:55 +0530

    fix: Replace tilde with $HOME

--- tabs/applications-setup/dwmtitus-setup.sh
+++ tabs/applications-setup/dwmtitus-setup.sh
@@ -146,7 +146,7 @@ configure_backgrounds() {
     BG_DIR="$HOME/Pictures/backgrounds"
 
     # Check if the ~/Pictures directory exists
-    if [ ! -d "~/Pictures" ]; then
+    if [ ! -d "$HOME/Pictures" ]; then
         # If it doesn't exist, print an error message and return with a status of 1 (indicating failure)
         echo "Pictures directory does not exist"
         mkdir ~/Pictures
