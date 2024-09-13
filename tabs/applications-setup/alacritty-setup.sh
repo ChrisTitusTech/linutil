f084efe5f7dde6fa237c69b596bad4eb04535bf7
Mayur Raut <95993675+RazoBeckett@users.noreply.github.com>
2024-09-13 22:59:08 +0530

    feat(alacritty): Add migration support
    auto migrate from YAML to TOML for version newer or equal to 0.13.0 of
    alacritty

--- tabs/applications-setup/alacritty-setup.sh
+++ tabs/applications-setup/alacritty-setup.sh
@@ -23,6 +23,17 @@ setupAlacritty() {
     mkdir -p "${HOME}/.config/alacritty/"
     wget -O "${HOME}/.config/alacritty/alacritty.toml" "https://github.com/ChrisTitusTech/dwm-titus/raw/main/config/alacritty/alacritty.toml"
     wget -O "${HOME}/.config/alacritty/nordic.toml" "https://github.com/ChrisTitusTech/dwm-titus/raw/main/config/alacritty/nordic.toml"
+
+    alacritty_version=$(alacritty --version | awk '{print $2}')
+    if [ "$(printf '%s\n' "$alacritty_version" "0.13.0" | sort -V | head -n1)" = "0.13.0" ]; then
+        echo "Alacritty version is newer than 0.13.0, migrating config files..."
+        if alacritty migrate; then
+            # using 'command' to avoid alias
+            command rm -f "${HOME}/.config/alacritty/*.yml"
+        else
+            echo "Failed to migrate alacritty config files."
+        fi
+    fi
 }
 
 checkEnv
