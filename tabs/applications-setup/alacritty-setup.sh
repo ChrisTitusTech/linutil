cdb6f3dcc8f999061e7853b36684b0900308bf27
Adam Perkowski <adas1per@protonmail.com>
2024-09-13 20:15:36 +0200

    Add migration support
    Auto migrate from YAML to TOML for alacritty >= 0.13.0
    
    Co-authored-by: RazoBeckett <95993675+RazoBeckett@users.noreply.github.com>

--- tabs/applications-setup/alacritty-setup.sh
+++ tabs/applications-setup/alacritty-setup.sh
@@ -17,20 +17,23 @@ setupAlacritty() {
         echo "alacritty is already installed."
     fi
 
-    alacritty_version=$(alacritty --version | awk '{print $2}')
-    if [ "$(printf '%s\n' "$alacritty_version" "0.13.0" | sort -V | head -n1)" != "0.13.0" ]; then        # Check if alacritty is >= 0.13.0  https://alacritty.org/changelog_0_13_0.html#Changed
-        echo
-        echo "Only alacritty >= 0.13.0 is supported."
-        exit 1
-    fi
-
     echo "Copy alacritty config files"
     if [ -d "${HOME}/.config/alacritty" ]; then
         cp -r "${HOME}/.config/alacritty" "${HOME}/.config/alacritty-bak"
     fi
     mkdir -p "${HOME}/.config/alacritty/"
-    wget -O "${HOME}/.config/alacritty/alacritty.toml" "https://github.com/ChrisTitusTech/dwm-titus/raw/main/config/alacritty/alacritty.toml"
-    wget -O "${HOME}/.config/alacritty/nordic.toml" "https://github.com/ChrisTitusTech/dwm-titus/raw/main/config/alacritty/nordic.toml"
+    wget -O "${HOME}/.config/alacritty/alacritty.yml" "https://github.com/ChrisTitusTech/dwm-titus/raw/main/config/alacritty/alacritty.yml"
+    wget -O "${HOME}/.config/alacritty/nordic.yml" "https://github.com/ChrisTitusTech/dwm-titus/raw/main/config/alacritty/nordic.yml"
+
+    alacritty_version=$(alacritty --version | awk '{print $2}')
+    if [ "$(printf '%s\n' "$alacritty_version" "0.13.0" | sort -V | head -n1)" = "0.13.0" ]; then    # Check if alacritty is < 0.13.0  https://alacritty.org/changelog_0_13_0.html#Changed
+        echo "Alacritty version is gearter or equal to 0.13.0, migrating config files..."
+        if alacritty migrate; then
+            command rm -f "${HOME}/.config/alacritty/*.yml"                                          # Using 'command' to avoid alias
+        else
+            echo "Failed to migrate alacritty config files."
+        fi
+    fi
 }
 
 checkEnv
