142d0842f1a106b15ad9cb03f025ef44b9b76870
Adam Perkowski <adas1per@protonmail.com>
2024-09-13 16:19:30 +0200

    alacritty version check

--- tabs/applications-setup/alacritty-setup.sh
+++ tabs/applications-setup/alacritty-setup.sh
@@ -16,6 +16,14 @@ setupAlacritty() {
     else
         echo "alacritty is already installed."
     fi
+
+    alacritty_version=$(alacritty --version | awk '{print $2}')
+    if [ "$(printf '%s\n' "$alacritty_version" "0.13.0" | sort -V | head -n1)" != "0.13.0" ]; then        # Check if alacritty is >= 0.13.0  https://alacritty.org/changelog_0_13_0.html#Changed
+        echo
+        echo "Only alacritty >= 0.13.0 is supported."
+        exit 1
+    fi
+
     echo "Copy alacritty config files"
     if [ -d "${HOME}/.config/alacritty" ]; then
         cp -r "${HOME}/.config/alacritty" "${HOME}/.config/alacritty-bak"
