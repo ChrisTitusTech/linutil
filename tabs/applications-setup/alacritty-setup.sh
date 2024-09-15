3ea8cf5b16b524f130d0846bd923aded39a6d108
nnyyxxxx <nnyyxxxx@users.noreply.github.com>
2024-09-15 01:27:23 -0400

    Add revert functionality to alacrittySetup

--- tabs/applications-setup/alacritty-setup.sh
+++ tabs/applications-setup/alacritty-setup.sh
@@ -25,6 +25,36 @@ setupAlacritty() {
     wget -O "${HOME}/.config/alacritty/nordic.toml" "https://github.com/ChrisTitusTech/dwm-titus/raw/main/config/alacritty/nordic.toml"
 }
 
-checkEnv
-checkEscalationTool
-setupAlacritty
+revertAlacritty() {
+    echo "Reverting Alacritty setup..."
+    if [ -d "${HOME}/.config/alacritty-bak" ]; then
+        rm -rf "${HOME}/.config/alacritty"
+        mv "${HOME}/.config/alacritty-bak" "${HOME}/.config/alacritty"
+        
+        if command_exists alacritty; then
+            case ${PACKAGER} in
+                pacman)
+                    $ESCALATION_TOOL ${PACKAGER} -R --noconfirm alacritty
+                    ;;
+                *)
+                    $ESCALATION_TOOL ${PACKAGER} remove -y alacritty
+                    ;;
+            esac
+            echo "Alacritty uninstalled."
+        fi
+    else
+        echo "No backup found. Nothing to revert."
+    fi
+}
+
+run() {
+    checkEnv
+    checkEscalationTool
+    setupAlacritty
+}
+
+revert() {
+    checkEnv
+    checkEscalationTool
+    revertAlacritty
+}
