687ad1ffde8e37d4ca1b01aec0cc0a209736958d
Angaddeep Singh <angaddeep28@proton.me>
2024-09-15 18:47:28 +0530

    Added Virtualization Setup

---
+++ tabs/system-setup/fedora/virtualization.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -e
+
+. ../../common-script.sh
+
+# Install virtualization tools to enable virtual machines
+configureVirtualization() {
+    case $PACKAGER in
+        dnf)
+             echo "Installing virtualization tools..."
+            $ESCALATION_TOOL "$PACKAGER" install -y @virtualization 
+            echo "Installed virtualization tools..."
+            ;;
+        *)
+            echo "Unsupported distribution: $DTYPE"
+            ;;
+    esac
+}
+
+checkEnv
+configureVirtualization
