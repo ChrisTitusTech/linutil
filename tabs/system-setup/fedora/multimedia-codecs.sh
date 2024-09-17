009ad3af0b574c428d1a27d397afa8fffdca4709
Angaddeep Singh <angaddeep28@proton.me>
2024-09-17 16:33:24 +0530

    update

--- tabs/system-setup/fedora/multimedia-codecs.sh
+++ tabs/system-setup/fedora/multimedia-codecs.sh
@@ -6,11 +6,11 @@ multimedia() {
     case $PACKAGER in
         dnf)
             if [[ -e /etc/yum.repos.d/rpmfusion-free.repo && -e /etc/yum.repos.d/rpmfusion-nonfree.repo ]]; then
-            echo "Installing Multimedia Codecs"
+            printf "%b\n" ${YELLOW}"Installing Multimedia Codecs...${RC}"
             $ESCALATION_TOOL "$PACKAGER" swap ffmpeg-free ffmpeg --allowerasing -y
             $ESCALATION_TOOL "$PACKAGER" update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
             $ESCALATION_TOOL "$PACKAGER" update @sound-and-video -y
-            echo "Multimedia Codecs Installed"
+            printf "%b\n" "${GREEN}Multimedia Codecs Installed...${RC}"
             fi
             ;;
         *)
