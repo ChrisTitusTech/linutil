aab66ffada67ddd830a6624b189d27c94450a535
Mashruk Kabir <120426030+mashrukk@users.noreply.github.com>
2024-09-18 07:52:22 +0000

    Update tabs/system-setup/system-cleanup.sh
    Co-authored-by: Nyx <144965845+nnyyxxxx@users.noreply.github.com>

--- tabs/system-setup/system-cleanup.sh
+++ tabs/system-setup/system-cleanup.sh
@@ -4,9 +4,9 @@
 
 cleanup_system() {
     printf "%b\n" "${YELLOW}Performing system cleanup...${RC}"
-    case $PACKAGER in
-        apt-get|nala)
-            $ESCALATION_TOOL "$PACKAGER" clean
+    case "$PACKAGER" in
+        apt|nala)
+            "$ESCALATION_TOOL" "$PACKAGER" clean
             $ESCALATION_TOOL "$PACKAGER" autoremove -y
             $ESCALATION_TOOL "$PACKAGER" autoclean
             $ESCALATION_TOOL du -h /var/cache/apt
@@ -20,19 +20,13 @@ cleanup_system() {
         dnf)
             $ESCALATION_TOOL "$PACKAGER" clean all
             $ESCALATION_TOOL "$PACKAGER" autoremove -y
-            $ESCALATION_TOOL "$PACKAGER" remove "$(dnf repoquery --extras --exclude=kernel,kernel-\*)" -y
             ;;
         pacman)
             $ESCALATION_TOOL "$PACKAGER" -Sc --noconfirm
             $ESCALATION_TOOL "$PACKAGER" -Rns "$(pacman -Qtdq)" --noconfirm
-            if command_exists paru >/dev/null 2>&1; then
-                paru -c
-            elif command_exists yay >/dev/null 2>&1; then
-                yay -c
-            fi
             ;;
         *)
-            printf "%b\n" "${RED}Unsupported package manager.${RC}"
+            printf "%b\n" "${RED}Unsupported package manager: $PACKAGER${RC}"
             return 1
             ;;
     esac
@@ -48,13 +42,13 @@ common_cleanup() {
 }
 
 clean_data() {
-    printf "%b" "${YELLOW}Clean up old cache files and empty the trash? (y/N) ${RC}"
+    printf "%b\n" "${YELLOW}Clean up old cache files and empty the trash? (y/N): ${RC}"
     read -r clean_response
     case $clean_response in
         [Yy]*)
             printf "%b\n" "${YELLOW}Cleaning up old cache files and emptying trash...${RC}"
-            find ~/.cache/ -type f -atime +5 -delete
-            find ~/.local/share/Trash -mindepth 1 -delete
+            find "$HOME/.cache/" -type f -atime +5 -delete
+            find "$HOME/.local/share/Trash" -mindepth 1 -delete
             printf "%b\n" "${GREEN}Cache and trash cleanup completed.${RC}"
             ;;
         *)
