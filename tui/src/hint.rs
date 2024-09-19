1ea326747a9b54e5bf7bdd54e09d429600747d12
cartercanedy <99052281+cartercanedy@users.noreply.github.com>
2024-09-18 19:00:09 -0700

    fix: Fixup preview rendering and logic of preview key binding being shown in shortcut list (#387)
    * always check if selection is a command if list is focused
    
    * clear float widget render area before rendering content
    
    * Update hint.rs
    
    * Update floating_text.rs
    
    Thanks to @jeevithakannan2 for idea
    
    ---------
    
    Co-authored-by: Chris Titus <contact@christitus.com>

--- tui/src/hint.rs
+++ tui/src/hint.rs
@@ -114,9 +114,11 @@ pub fn draw_shortcuts(state: &AppState, frame: &mut Frame, area: Rect) {
             scope_name: "Search bar",
             hints: vec![Shortcut::new(vec!["Enter"], "Finish search")],
         },
+
         Focus::List => {
             let mut hints = Vec::new();
             hints.push(Shortcut::new(vec!["q", "CTRL-c"], "Exit linutil"));
+
             if state.at_root() {
                 hints.push(Shortcut::new(vec!["h", "Left", "Tab"], "Focus tab list"));
                 hints.push(get_list_item_shortcut(state));
@@ -136,15 +138,18 @@ pub fn draw_shortcuts(state: &AppState, frame: &mut Frame, area: Rect) {
                 }
                 hints.push(Shortcut::new(vec!["Tab"], "Focus tab list"));
             };
+
             hints.push(Shortcut::new(vec!["k", "Up"], "Select item above"));
             hints.push(Shortcut::new(vec!["j", "Down"], "Select item below"));
             hints.push(Shortcut::new(vec!["t"], "Next theme"));
             hints.push(Shortcut::new(vec!["T"], "Previous theme"));
+
             ShortcutList {
                 scope_name: "Item list",
                 hints,
             }
         }
+
         Focus::TabList => ShortcutList {
             scope_name: "Tab list",
             hints: vec![
@@ -156,6 +161,7 @@ pub fn draw_shortcuts(state: &AppState, frame: &mut Frame, area: Rect) {
                 Shortcut::new(vec!["T"], "Previous theme"),
             ],
         },
+
         Focus::FloatingWindow(ref float) => float.get_shortcut_list(),
     }
     .draw(frame, area);
