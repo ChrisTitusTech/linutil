404cb606a577a4c6d1f0ebad5bf1938394403142
Guru Swarupa <112751363+guruswarupa@users.noreply.github.com>
2024-09-19 19:12:30 +0530

    Update 7-grub-theme.sh

--- tabs/system-setup/7-grub-theme.sh
+++ tabs/system-setup/7-grub-theme.sh
@@ -2,6 +2,7 @@
 . ../common-script.sh
 
 themeinstall(){
+    mkdir -p "$HOME/.local/share"
     cd "$HOME/.local/share" && git clone "https://github.com/ChrisTitusTech/Top-5-Bootloader-Themes"
     cd "$HOME/.local/share/Top-5-Bootloader-Themes"
     $ESCALATION_TOOL ./install.sh
