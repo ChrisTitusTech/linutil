588123d4adf18bebdeb404ab66b19c23dcb6347d
nnyyxxxx <nnyyxxxx@users.noreply.github.com>
2024-09-19 18:50:47 -0400

    Phase 2: Bottles

--- tabs/applications-setup/bottles-setup.sh
+++ tabs/applications-setup/bottles-setup.sh
@@ -3,10 +3,16 @@
 . ../common-script.sh
 
 install_bottles() {
-    printf "%b\n" "${YELLOW}Installing Bottles...${RC}"
-    . ./setup-flatpak.sh
-    flatpak install -y flathub com.usebottles.bottles
-    printf "%b\n" "${GREEN}Bottles installed successfully. Restart the system to apply changes...${RC}"
+    printf "%b" "${YELLOW}Do you want to install Bottles? (Y/n): ${RC}"
+    read -r install_choice
+    if [ "$install_choice" != "n" ] && [ "$install_choice" != "N" ]; then
+        printf "%b\n" "${YELLOW}Installing Bottles...${RC}"
+        . ./setup-flatpak.sh
+        flatpak install -y flathub com.usebottles.bottles
+        printf "%b\n" "${GREEN}Bottles installed successfully.${RC}"
+    else
+        printf "%b\n" "${GREEN}Skipping Bottles installation.${RC}"
+    fi
 }
 
 checkEnv
