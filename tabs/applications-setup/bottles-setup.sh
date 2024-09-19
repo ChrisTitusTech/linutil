875f8f886a72d200c96d746ebb3778db26859a43
Guru Swarupa <112751363+guruswarupa@users.noreply.github.com>
2024-09-19 13:26:37 +0530

    Update tabs/applications-setup/bottles-setup.sh
    Co-authored-by: Liam <33645555+lj3954@users.noreply.github.com>

--- tabs/applications-setup/bottles-setup.sh
+++ tabs/applications-setup/bottles-setup.sh
@@ -4,29 +4,7 @@
 
 install_bottles() {
     printf "%b\n" "${YELLOW}Installing Bottles...${RC}"
-    if ! command_exists flatpak; then
-    case $PACKAGER in
-        pacman)
-            $ESCALATION_TOOL ${PACKAGER} -Syu --noconfirm flatpak         
-            ;;
-        apt-get)
-            $ESCALATION_TOOL ${PACKAGER} update && $ESCALATION_TOOL ${PACKAGER} install -y flatpak
-            ;;
-        dnf)
-            $ESCALATION_TOOL ${PACKAGER} install -y flatpak
-            ;;
-        zypper)
-            $ESCALATION_TOOL ${PACKAGER} install flatpak
-            ;;
-        *)
-            printf "%b\n" "${RED}Your Linux distribution is not supported by this script.${RC}"
-            printf "%b\n" "${YELLOW}You can try installing Bottles using Flatpak manually:${RC}"
-            printf "%b\n" "1. Install Flatpak: https://flatpak.org/setup/"
-            printf "%b\n" "2. Install Bottles: flatpak install flathub com.usebottles.bottles"
-            ;;
-    esac
-    fi
-    flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    . ./setup-flatpak.sh
     flatpak install -y flathub com.usebottles.bottles
     printf "%b\n" "${GREEN}Bottles installed successfully. Restart the system to apply changes...${RC}"
 }
