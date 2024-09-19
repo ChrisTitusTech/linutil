ed2600e26c727496e7ebccd033e9ae2dbd2b35ce
Mashruk Kabir <120426030+mashrukk@users.noreply.github.com>
2024-09-19 23:30:16 +0600

    Update browser-setup.sh

--- tabs/applications-setup/browser-setup.sh
+++ tabs/applications-setup/browser-setup.sh
@@ -30,26 +30,26 @@ install_chrome() {
 }
 
 install_thorium() {
-	printf "%b\n" "${YELLOW}Installing Thorium Browser...${RC}"
-	case $PACKAGER in
-		apt-get|nala)
-			"$ESCALATION_TOOL" rm -fv /etc/apt/sources.list.d/thorium.list
-			"$ESCALATION_TOOL" wget --no-hsts -P /etc/apt/sources.list.d/ http://dl.thorium.rocks/debian/dists/stable/thorium.list
-			"$ESCALATION_TOOL" "$PACKAGER" update
-			"$ESCALATION_TOOL" "$PACKAGER" install -y thorium-browser
-			;;
-		zypper|dnf)
-		        url=$(curl -s https://api.github.com/repos/Alex313031/Thorium/releases/latest | grep -oP '(?<=browser_download_url": ")[^"]*\.rpm')
-                        echo $url && curl -L $url -o thorium-latest.rpm
-                        "$ESCALATION_TOOL" rpm -i thorium-latest.rpm && rm thorium-latest.rpm
-			;;
-		pacman)
-			"$AUR_HELPER" -S --needed --noconfirm thorium-browser-bin
-			;;
+    printf "%b\n" "${YELLOW}Installing Thorium Browser...${RC}"
+    case $PACKAGER in
+	apt-get|nala)
+	    "$ESCALATION_TOOL" rm -fv /etc/apt/sources.list.d/thorium.list
+            "$ESCALATION_TOOL" wget --no-hsts -P /etc/apt/sources.list.d/ http://dl.thorium.rocks/debian/dists/stable/thorium.list
+	    "$ESCALATION_TOOL" "$PACKAGER" update
+	    "$ESCALATION_TOOL" "$PACKAGER" install -y thorium-browser
+		;;
+	zypper|dnf)
+	    url=$(curl -s https://api.github.com/repos/Alex313031/Thorium/releases/latest | grep -oP '(?<=browser_download_url": ")[^"]*\.rpm')
+            echo $url && curl -L $url -o thorium-latest.rpm
+            "$ESCALATION_TOOL" rpm -i thorium-latest.rpm && rm thorium-latest.rpm
+		;;
+	pacman)
+	     "$AUR_HELPER" -S --needed --noconfirm thorium-browser-bin
+		;;
 		*)
-			printf "%b\n" "${RED}Unsupported package manager. Please install Thorium manually.${RC}"
-			exit 1
-			;;
+	        printf "%b\n" "${RED}Unsupported package manager. Please install Thorium manually.${RC}"
+		exit 1
+		;;
 	esac
 	printf "%b\n" "${GREEN}Thorium Browser installed successfully!${RC}"
 }
