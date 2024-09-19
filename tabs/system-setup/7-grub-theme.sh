94012880f18d8f8e525332fb7257021e15f10391
Chris Titus <contact@christitus.com>
2024-09-19 12:21:31 -0500

    Update tabs/system-setup/7-grub-theme.sh
    Co-authored-by: Adam Perkowski <adas1per@protonmail.com>

--- tabs/system-setup/7-grub-theme.sh
+++ tabs/system-setup/7-grub-theme.sh
@@ -5,7 +5,7 @@ themeinstall(){
     mkdir -p "$HOME/.local/share"
     cd "$HOME/.local/share" && git clone "https://github.com/ChrisTitusTech/Top-5-Bootloader-Themes"
     cd "$HOME/.local/share/Top-5-Bootloader-Themes"
-    $ESCALATION_TOOL ./install.sh
+    "$ESCALATION_TOOL" ./install.sh
 }
 
 checkEnv
