bfaf0dbaf01e67d6015b9f2918a7a96352d62761
Guru Swarupa <112751363+guruswarupa@users.noreply.github.com>
2024-09-21 23:57:13 +0530

    added adb setup and uad setup

---
+++ core/tabs/applications-setup/android-debloat.sh
@@ -0,0 +1,44 @@
+#!/bin/sh -e
+
+. ../common-script.sh
+
+install_adb() {
+    if ! command_exists adb ; then
+        printf "%b\n" "${YELLOW}Installing ADB..${RC}."
+        case "$PACKAGER" in
+            apt-get|nala)
+                "$ESCALATION_TOOL" "$PACKAGER" install android-sdk-platform-tools
+                ;;
+            zypper)
+                "$ESCALATION_TOOL" "$PACKAGER" install android-tools
+                ;;
+            pacman)
+                "$ESCALATION_TOOL" "$PACKAGER" -S android-tools
+                ;;
+            yum)
+                "$ESCALATION_TOOL" "$PACKAGER" install android-tools
+                ;;
+            *)
+                printf "%b\n" "${RED}The script does not support your Distro. Install manually..${RC}"
+                ;;
+        esac
+    else
+        printf "%b\n" "${GREEN}ADB is already installed.${RC}"
+    fi
+}
+
+install_universal_android_debloater() {
+    if ! command_exists uad; then
+        printf "%b\n" "${YELLOW}Installing ADB..${RC}."
+        curl -sSLo "${HOME}/uad" "https://github.com/Universal-Debloater-Alliance/universal-android-debloater-next-generation/releases/download/v1.1.0/uad-ng-linux"
+        "$ESCALATION_TOOL" chmod +x "${HOME}/uad"
+        "$ESCALATION_TOOL" mv "${HOME}/uad" /usr/local/bin/uad
+    else
+        printf "%b\n" "${GREEN}Universal Android Debloater is already installed. Run 'uad' command to execute...${RC}"      
+    fi
+}                   
+
+checkEnv
+checkEscalationTool
+install_adb
+install_universal_android_debloater 
