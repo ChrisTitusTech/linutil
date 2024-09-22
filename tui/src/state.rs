bac963eea0547ff0318fa9bfaca721d9b758c26a
Adam Perkowski <adas1per@protonmail.com>
2024-09-22 21:01:23 +0200

    More space

--- tui/src/state.rs
+++ tui/src/state.rs
@@ -201,7 +201,7 @@ impl AppState {
 
         let list_chunks = Layout::default()
             .direction(Direction::Horizontal)
-            .constraints([Constraint::Percentage(85), Constraint::Percentage(15)].as_ref())
+            .constraints([Constraint::Percentage(70), Constraint::Percentage(30)].as_ref())
             .split(chunks[1]);
 
         self.filter.draw_searchbar(frame, chunks[0], &self.theme);
