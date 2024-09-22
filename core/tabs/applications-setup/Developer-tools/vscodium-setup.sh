d9e420c4d486826586ce2d0c3606c5c54539a163
Chris Titus <contact@christitus.com>
2024-09-22 17:18:46 -0500

    Update core/tabs/applications-setup/Developer-tools/vscodium-setup.sh
    Co-authored-by: Adam Perkowski <adas1per@protonmail.com>

--- core/tabs/applications-setup/Developer-tools/vscodium-setup.sh
+++ core/tabs/applications-setup/Developer-tools/vscodium-setup.sh
@@ -16,7 +16,7 @@ installVsCodium() {
                 "$ESCALATION_TOOL" rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
                 printf "%b\n" "[gitlab.com_paulcarroty_vscodium_repo]\nname=gitlab.com_paulcarroty_vscodium_repo\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | "$ESCALATION_TOOL" tee -a /etc/zypp/repos.d/vscodium.repo
                 "$ESCALATION_TOOL" "$PACKAGER" refresh
-                "$ESCALATION_TOOL" "$PACKAGER" install -y codium
+                "$ESCALATION_TOOL" "$PACKAGER" --non-interactive install codium
 
                 ;;
             pacman)
