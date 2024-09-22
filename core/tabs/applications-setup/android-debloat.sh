e79c3feb8f8ea4ae51f81c236dee7415725a6b32
Guru Swarupa <112751363+guruswarupa@users.noreply.github.com>
2024-09-22 13:32:41 +0000

    Update android-debloat.sh

--- core/tabs/applications-setup/android-debloat.sh
+++ core/tabs/applications-setup/android-debloat.sh
@@ -15,7 +15,7 @@ install_adb() {
             pacman)
                 "$ESCALATION_TOOL" "$PACKAGER" -S --noconfirm android-tools
                 ;;
-            rpm)
+            dnf)
                 "$ESCALATION_TOOL" "$PACKAGER" install -y android-tools
                 ;;
             *)
