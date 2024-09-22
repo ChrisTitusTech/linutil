072645bc9b0011c89076f97acb4c5784845d3c2b
Guru Swarupa <112751363+guruswarupa@users.noreply.github.com>
2024-09-22 17:58:52 +0000

    added communication apps to application setup + small fix in dev tools (#618)
    * added communication apps to application setup
    
    * Update core/tabs/applications-setup/communication-apps/thunderbird-setup.sh
    
    Co-authored-by: Adam Perkowski <adas1per@protonmail.com>
    
    * Update core/tabs/applications-setup/communication-apps/discord-setup.sh
    
    Co-authored-by: Adam Perkowski <adas1per@protonmail.com>
    
    * Update core/tabs/applications-setup/communication-apps/jitsi-meet-setup.sh
    
    Co-authored-by: Adam Perkowski <adas1per@protonmail.com>
    
    * Update core/tabs/applications-setup/communication-apps/signal-setup.sh
    
    Co-authored-by: Adam Perkowski <adas1per@protonmail.com>
    
    * Update core/tabs/applications-setup/communication-apps/telegram-setup.sh
    
    Co-authored-by: Adam Perkowski <adas1per@protonmail.com>
    
    * Update core/tabs/applications-setup/communication-apps/discord-setup.sh
    
    Co-authored-by: Adam Perkowski <adas1per@protonmail.com>
    
    * Update core/tabs/applications-setup/communication-apps/jitsi-meet-setup.sh
    
    Co-authored-by: Adam Perkowski <adas1per@protonmail.com>
    
    * add --needed
    
    * fix script name.
    
    * added task_list
    
    * fix sh -c
    
    * Update core/tabs/applications-setup/communication-apps/jitsi-setup.sh
    
    Co-authored-by: Nyx <144965845+nnyyxxxx@users.noreply.github.com>
    
    * speed up github desktop installation
    
    * Update core/tabs/applications-setup/communication-apps/zoom-setup.sh
    
    Co-authored-by: Nyx <144965845+nnyyxxxx@users.noreply.github.com>
    
    * Update core/tabs/applications-setup/tab_data.toml
    
    Co-authored-by: Nyx <144965845+nnyyxxxx@users.noreply.github.com>
    
    * Update core/tabs/applications-setup/tab_data.toml
    
    Co-authored-by: Adam Perkowski <adas1per@protonmail.com>
    
    ---------
    
    Co-authored-by: Adam Perkowski <adas1per@protonmail.com>
    Co-authored-by: Nyx <144965845+nnyyxxxx@users.noreply.github.com>

---
+++ core/tabs/applications-setup/communication-apps/signal-setup.sh
@@ -0,0 +1,37 @@
+#!/bin/sh -e
+
+. ../../common-script.sh
+
+installSignal() {
+    if ! command_exists signal; then
+        printf "%b\n" "${YELLOW}Installing Signal...${RC}"
+        case "$PACKAGER" in
+            apt-get|nala)
+                curl -fsSL https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
+                cat signal-desktop-keyring.gpg | "$ESCALATION_TOOL" tee /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
+                printf "%b\n" 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' | "$ESCALATION_TOOL" tee /etc/apt/sources.list.d/signal-xenial.list
+                "$ESCALATION_TOOL" "$PACKAGER" -y install signal-desktop
+                ;;
+            zypper)
+                "$ESCALATION_TOOL" "$PACKAGER" --non-interactive install signal-desktop
+                ;;
+            pacman)
+                "$ESCALATION_TOOL" "$PACKAGER" -S --noconfirm signal-desktop
+                ;;
+            dnf)
+                "$ESCALATION_TOOL" "$PACKAGER" copr enable luminoso/Signal-Desktop
+                "$ESCALATION_TOOL" "$PACKAGER" install -y signal-desktop 
+                ;;
+            *)
+                printf "%b\n" "${RED}Unsupported package manager: ""$PACKAGER""${RC}"
+                exit 1
+                ;;
+        esac
+    else
+        printf "%b\n" "${GREEN}Signal is already installed.${RC}"
+    fi
+}
+
+checkEnv
+checkEscalationTool
+installSignal
---
+++ core/tabs/applications-setup/communication-apps/slack-setup.sh
@@ -0,0 +1,25 @@
+#!/bin/sh -e
+
+. ../../common-script.sh
+
+installSlack() {
+    if ! command_exists slack; then
+        printf "%b\n" "${YELLOW}Installing Slack...${RC}"
+        case "$PACKAGER" in
+            pacman)
+                "$AUR_HELPER" -S --needed --noconfirm slack-desktop
+                ;;
+            *)  
+                . ./setup-flatpak.sh
+                flatpak install -y flathub com.slack.Slack
+                ;;
+        esac
+    else
+        printf "%b\n" "${GREEN}Slack is already installed.${RC}"
+    fi
+}
+
+checkEnv
+checkEscalationTool
+checkAURHelper
+installSlack
