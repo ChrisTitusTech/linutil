347567099e991f0d85c3a0f358fd270bdef08ad5
Guru Swarupa <112751363+guruswarupa@users.noreply.github.com>
2024-09-22 16:42:35 +0000

    Update core/tabs/applications-setup/communication-apps/telegram-setup.sh
    Co-authored-by: Adam Perkowski <adas1per@protonmail.com>

--- core/tabs/applications-setup/communication-apps/telegram-setup.sh
+++ core/tabs/applications-setup/communication-apps/telegram-setup.sh
@@ -19,7 +19,7 @@ installTelegram() {
                 "$ESCALATION_TOOL" "$PACKAGER" install -y telegram-desktop 
                 ;;
             *)
-                printf "%b\n" "${RED}Unsupported package manager. Please install Telegram manually.${RC}"
+                printf "%b\n" "${RED}Unsupported package manager: ""$PACKAGER""${RC}"
                 exit 1
                 ;;
         esac
