35c41e883ced6e0319638b660c6df6829de9a869
Nyx <144965845+nnyyxxxx@users.noreply.github.com>
2024-09-28 15:13:35 -0400

    add git as a dep (#649)
    Co-authored-by: nnyyxxxx <nnyyxxxx@users.noreply.github.com>

--- core/tabs/system-setup/arch/paru-setup.sh
+++ core/tabs/system-setup/arch/paru-setup.sh
@@ -7,7 +7,7 @@ installDepend() {
         pacman)
             if ! command_exists paru; then
                 printf "%b\n" "${YELLOW}Installing paru as AUR helper...${RC}"
-                "$ESCALATION_TOOL" "$PACKAGER" -S --needed --noconfirm base-devel
+                "$ESCALATION_TOOL" "$PACKAGER" -S --needed --noconfirm base-devel git
                 cd /opt && "$ESCALATION_TOOL" git clone https://aur.archlinux.org/paru.git && "$ESCALATION_TOOL" chown -R "$USER": ./paru
                 cd paru && makepkg --noconfirm -si
                 printf "%b\n" "${GREEN}Paru installed${RC}"
