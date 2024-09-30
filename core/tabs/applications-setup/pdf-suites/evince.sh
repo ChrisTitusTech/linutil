4d2c2d3ab737304007e8c46522094f0ff2d7c22e
Nyx <144965845+nnyyxxxx@users.noreply.github.com>
2024-09-30 16:52:15 -0400

    Separate the office suites and pdf suites (#650)
    * Separate the office suites and pdf suites
    
    * add --needed
    
    * move 2 scripts over to pdf-suites
    
    * fix hanging data entry
    
    * alphabetically sort entries
    
    * address carters concerns
    
    * fix compilation errors
    
    ---------
    
    Co-authored-by: nnyyxxxx <nnyyxxxx@users.noreply.github.com>

---
+++ core/tabs/applications-setup/pdf-suites/evince.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -e
+
+. ../common-script.sh
+
+installEvince() {
+    if ! command_exists evince; then
+        printf "%b\n" "${YELLOW}Installing Evince...${RC}"
+        case "$PACKAGER" in
+            pacman)
+                "$ESCALATION_TOOL" "$PACKAGER" -S --needed --noconfirm evince
+                ;;
+            *)
+                "$ESCALATION_TOOL" "$PACKAGER" install -y evince
+                ;;
+        esac
+    else
+        printf "%b\n" "${GREEN}Evince is already installed.${RC}"
+    fi
+}
+
+checkEnv
+checkEscalationTool
+installEvince
