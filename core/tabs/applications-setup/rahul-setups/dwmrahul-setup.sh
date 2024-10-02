da5e513326cbff38abfb5bb7ae18ccdb58238039
Rahul Jangir <78741215+aarjaycreation@users.noreply.github.com>
2024-10-02 21:18:24 +0530

    added rahul-setup folder

---
+++ core/tabs/applications-setup/rahul-setups/dwmrahul-setup.sh
@@ -0,0 +1,266 @@
+#!/bin/sh -e
+
+. ../../common-script.sh
+
+setupDWM() {
+    printf "%b\n" "${YELLOW}Installing DWM-Titus...${RC}"
+    case "$PACKAGER" in # Install pre-Requisites
+        pacman)
+            "$ESCALATION_TOOL" "$PACKAGER" -S --needed --noconfirm base-devel libx11 libxinerama libxft imlib2 libxcb git unzip flameshot lxappearance feh mate-polkit
+            ;;
+        apt-get|nala)
+            "$ESCALATION_TOOL" "$PACKAGER" install -y build-essential libx11-dev libxinerama-dev libxft-dev libimlib2-dev libx11-xcb-dev libfontconfig1 libx11-6 libxft2 libxinerama1 libxcb-res0-dev git unzip flameshot lxappearance feh mate-polkit
+            ;;
+        dnf)
+            "$ESCALATION_TOOL" "$PACKAGER" groupinstall -y "Development Tools"
+            "$ESCALATION_TOOL" "$PACKAGER" install -y libX11-devel libXinerama-devel libXft-devel imlib2-devel libxcb-devel unzip flameshot lxappearance feh mate-polkit # no need to include git here as it should be already installed via "Development Tools"
+            ;;
+        *)
+            printf "%b\n" "${RED}Unsupported package manager: ""$PACKAGER""${RC}"
+            exit 1
+            ;;
+    esac
+}
+
+makeDWM() {
+    cd "$HOME" && git clone https://github.com/ChrisTitusTech/dwm-titus.git # CD to Home directory to install dwm-titus
+    # This path can be changed (e.g. to linux-toolbox directory)
+    cd dwm-titus/ # Hardcoded path, maybe not the best.
+    "$ESCALATION_TOOL" make clean install # Run make clean install
+}
+
+install_nerd_font() {
+    FONT_DIR="$HOME/.local/share/fonts"
+    FONT_ZIP="$FONT_DIR/Meslo.zip"
+    FONT_URL="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Meslo.zip"
+    FONT_INSTALLED=$(fc-list | grep -i "Meslo")
+
+    # Check if Meslo Nerd-font is already installed
+    if [ -n "$FONT_INSTALLED" ]; then
+        printf "%b\n" "${GREEN}Meslo Nerd-fonts are already installed.${RC}"
+        return 0
+    fi
+
+    printf "%b\n" "${YELLOW}Installing Meslo Nerd-fonts${RC}"
+
+    # Create the fonts directory if it doesn't exist
+    if [ ! -d "$FONT_DIR" ]; then
+        mkdir -p "$FONT_DIR" || {
+            printf "%b\n" "${RED}Failed to create directory: $FONT_DIR${RC}"
+            return 1
+        }
+    else
+        printf "%b\n" "${GREEN}$FONT_DIR exists, skipping creation.${RC}"
+    fi
+
+    # Check if the font zip file already exists
+    if [ ! -f "$FONT_ZIP" ]; then
+        # Download the font zip file
+        curl -sSLo "$FONT_ZIP" "$FONT_URL" || {
+            printf "%b\n" "${RED}Failed to download Meslo Nerd-fonts from $FONT_URL${RC}"
+            return 1
+        }
+    else
+        printf "%b\n" "${GREEN}Meslo.zip already exists in $FONT_DIR, skipping download.${RC}"
+    fi
+
+    # Unzip the font file if it hasn't been unzipped yet
+    if [ ! -d "$FONT_DIR/Meslo" ]; then
+        mkdir -p "$FONT_DIR/Meslo" || {
+            printf "%b\n" "${RED}Failed to create directory: $FONT_DIR/Meslo${RC}"
+            return 1
+        }
+        unzip "$FONT_ZIP" -d "$FONT_DIR" || {
+            printf "%b\n" "${RED}Failed to unzip $FONT_ZIP${RC}"
+            return 1
+        }
+    else
+        printf "%b\n" "${GREEN}Meslo font files already unzipped in $FONT_DIR, skipping unzip.${RC}"
+    fi
+
+    # Remove the zip file
+    rm "$FONT_ZIP" || {
+        printf "%b\n" "${RED}Failed to remove $FONT_ZIP${RC}"
+        return 1
+    }
+
+    # Rebuild the font cache
+    fc-cache -fv || {
+        printf "%b\n" "${RED}Failed to rebuild font cache${RC}"
+        return 1
+    }
+
+    printf "%b\n" "${GREEN}Meslo Nerd-fonts installed successfully${RC}"
+}
+
+picom_animations() {
+    # clone the repo into .local/share & use the -p flag to avoid overwriting that dir
+    mkdir -p "$HOME/.local/share/"
+    if [ ! -d "$HOME/.local/share/ftlabs-picom" ]; then
+        if ! git clone https://github.com/FT-Labs/picom.git "$HOME/.local/share/ftlabs-picom"; then
+            printf "%b\n" "${RED}Failed to clone the repository${RC}"
+            return 1
+        fi
+    else
+        printf "%b\n" "${GREEN}Repository already exists, skipping clone${RC}"
+    fi
+
+    cd "$HOME/.local/share/ftlabs-picom" || { printf "%b\n" "${RED}Failed to change directory to picom${RC}"; return 1; }
+
+    # Build the project
+    if ! meson setup --buildtype=release build; then
+        printf "%b\n" "${RED}Meson setup failed${RC}"
+        return 1
+    fi
+
+    if ! ninja -C build; then
+        printf "%b\n" "${RED}Ninja build failed${RC}"
+        return 1
+    fi
+
+    # Install the built binary
+    if ! "$ESCALATION_TOOL" ninja -C build install; then
+        printf "%b\n" "${RED}Failed to install the built binary${RC}"
+        return 1
+    fi
+
+    printf "%b\n" "${GREEN}Picom animations installed successfully${RC}"
+}
+
+clone_config_folders() {
+    # Ensure the target directory exists
+    [ ! -d ~/.config ] && mkdir -p ~/.config
+
+    # Iterate over all directories in config/*
+    for dir in config/*/; do
+        # Extract the directory name
+        dir_name=$(basename "$dir")
+
+        # Clone the directory to ~/.config/
+        if [ -d "$dir" ]; then
+            cp -r "$dir" ~/.config/
+            printf "%b\n" "${GREEN}Cloned $dir_name to ~/.config/${RC}"
+        else
+            printf "%b\n" "${RED}Directory $dir_name does not exist, skipping${RC}"
+        fi
+    done
+}
+
+configure_backgrounds() {
+    # Set the variable PIC_DIR which stores the path for images
+    PIC_DIR="$HOME/Pictures"
+
+    # Set the variable BG_DIR to the path where backgrounds will be stored
+    BG_DIR="$PIC_DIR/backgrounds"
+
+    # Check if the ~/Pictures directory exists
+    if [ ! -d "$PIC_DIR" ]; then
+        # If it doesn't exist, print an error message and return with a status of 1 (indicating failure)
+        printf "%b\n" "${RED}Pictures directory does not exist${RC}"
+        mkdir ~/Pictures
+        printf "%b\n" "${GREEN}Directory was created in Home folder${RC}"
+    fi
+
+    # Check if the backgrounds directory (BG_DIR) exists
+    if [ ! -d "$BG_DIR" ]; then
+        # If the backgrounds directory doesn't exist, attempt to clone a repository containing backgrounds
+        if ! git clone https://github.com/ChrisTitusTech/nord-background.git "$PIC_DIR/nord-background"; then
+            # If the git clone command fails, print an error message and return with a status of 1
+            printf "%b\n" "${RED}Failed to clone the repository${RC}"
+            return 1
+        fi
+        # Rename the cloned directory to 'backgrounds'
+        mv "$PIC_DIR/nord-background" "$PIC_DIR/backgrounds"
+        # Print a success message indicating that the backgrounds have been downloaded
+        printf "%b\n" "${GREEN}Downloaded desktop backgrounds to $BG_DIR${RC}"    
+    else
+        # If the backgrounds directory already exists, print a message indicating that the download is being skipped
+        printf "%b\n" "${GREEN}Path $BG_DIR exists for desktop backgrounds, skipping download of backgrounds${RC}"
+    fi
+}
+
+setupDisplayManager() {
+    printf "%b\n" "${YELLOW}Setting up Xorg${RC}"
+    case "$PACKAGER" in
+        pacman)
+            "$ESCALATION_TOOL" "$PACKAGER" -S --needed --noconfirm xorg-xinit xorg-server
+            ;;
+        apt-get|nala)
+            "$ESCALATION_TOOL" "$PACKAGER" install -y xorg xinit
+            ;;
+        dnf)
+            "$ESCALATION_TOOL" "$PACKAGER" install -y xorg-x11-xinit xorg-x11-server-Xorg
+            ;;
+        *)
+            printf "%b\n" "${RED}Unsupported package manager: "$PACKAGER"${RC}"
+            exit 1
+            ;;
+    esac
+    printf "%b\n" "${GREEN}Xorg installed successfully${RC}"
+    printf "%b\n" "${YELLOW}Setting up Display Manager${RC}"
+    currentdm="none"
+    for dm in gdm sddm lightdm; do
+        if systemctl is-active --quiet "$dm.service"; then
+            currentdm="$dm"
+            break
+        fi
+    done
+    printf "%b\n" "${GREEN}Current display manager: $currentdm${RC}"
+    if [ "$currentdm" = "none" ]; then
+        printf "%b\n" "${YELLOW}--------------------------${RC}" 
+        printf "%b\n" "${YELLOW}Pick your Display Manager ${RC}" 
+        printf "%b\n" "${YELLOW}1. SDDM ${RC}" 
+        printf "%b\n" "${YELLOW}2. LightDM ${RC}" 
+        printf "%b\n" "${YELLOW}3. GDM ${RC}" 
+        printf "%b\n" "${YELLOW} ${RC}" 
+        printf "%b" "${YELLOW}Please select one: ${RC}"
+        read -r DM
+        case "$PACKAGER" in
+            pacman)
+                "$ESCALATION_TOOL" "$PACKAGER" -S --needed --noconfirm "$DM"
+                ;;
+            apt-get|nala)
+                "$ESCALATION_TOOL" "$PACKAGER" install -y "$DM"
+                ;;
+            dnf)
+                "$ESCALATION_TOOL" "$PACKAGER" install -y "$DM"
+                ;;
+            *)
+                printf "%b\n" "${RED}Unsupported package manager: "$PACKAGER"${RC}"
+                exit 1
+                ;;
+        esac
+        printf "%b\n" "${GREEN}$DM installed successfully${RC}"
+        systemctl enable "$DM"
+        
+    fi
+}
+
+install_slstatus() {
+    printf "Do you want to install slstatus? (y/N): " # using printf instead of 'echo' to avoid newline, -n flag for 'echo' is not supported in POSIX
+    read -r response # -r flag to prevent backslashes from being interpreted
+    if [ "$response" = "y" ] || [ "$response" = "Y" ]; then
+        printf "%b\n" "${YELLOW}Installing slstatus${RC}"
+        cd "$HOME/dwm-titus/slstatus" || { printf "%b\n" "${RED}Failed to change directory to slstatus${RC}"; return 1; }
+        if "$ESCALATION_TOOL" make clean install; then
+            printf "%b\n" "${GREEN}slstatus installed successfully${RC}"
+        else
+            printf "%b\n" "${RED}Failed to install slstatus${RC}"
+            return 1
+        fi
+    else
+        printf "%b\n" "${GREEN}Skipping slstatus installation${RC}"
+    fi
+    cd "$HOME"
+}
+
+checkEnv
+checkEscalationTool
+setupDisplayManager
+setupDWM
+makeDWM
+install_slstatus
+install_nerd_font
+picom_animations
+clone_config_folders
+configure_backgrounds
---
+++ core/tabs/applications-setup/rahul-setups/mybash-setup.sh
@@ -0,0 +1,110 @@
+#!/bin/sh -e
+
+. ../../common-script.sh
+
+gitpath="$HOME/.local/share/mybash"
+
+installDepend() {
+    if ! command_exists bash bash-completion tar bat tree unzip fontconfig git; then
+    printf "%b\n" "${YELLOW}Installing Bash...${RC}"
+    case "$PACKAGER" in
+        pacman)
+            "$ESCALATION_TOOL" "$PACKAGER" -S --needed --noconfirm bash bash-completion tar bat tree unzip fontconfig git
+            ;;
+        *)
+            "$ESCALATION_TOOL" "$PACKAGER" install -y bash bash-completion tar bat tree unzip fontconfig git
+            ;;
+    esac
+    fi
+}
+
+cloneMyBash() {
+    # Check if the dir exists before attempting to clone into it.
+    if [ -d "$gitpath" ]; then
+        rm -rf "$gitpath"
+    fi
+    mkdir -p "$HOME/.local/share" # Only create the dir if it doesn't exist.
+    cd "$HOME" && git clone https://github.com/ChrisTitusTech/mybash.git "$gitpath"
+}
+
+installFont() {
+    # Check to see if the MesloLGS Nerd Font is installed (Change this to whatever font you would like)
+    FONT_NAME="MesloLGS Nerd Font Mono"
+    if fc-list :family | grep -iq "$FONT_NAME"; then
+        printf "%b\n" "${GREEN}Font '$FONT_NAME' is installed.${RC}"
+    else
+        printf "%b\n" "${YELLOW}Installing font '$FONT_NAME'${RC}"
+        # Change this URL to correspond with the correct font
+        FONT_URL="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Meslo.zip"
+        FONT_DIR="$HOME/.local/share/fonts"
+        TEMP_DIR=$(mktemp -d)
+        curl -sSLo "$TEMP_DIR"/"${FONT_NAME}".zip "$FONT_URL"
+        unzip "$TEMP_DIR"/"${FONT_NAME}".zip -d "$TEMP_DIR"
+        mkdir -p "$FONT_DIR"/"$FONT_NAME"
+        mv "${TEMP_DIR}"/*.ttf "$FONT_DIR"/"$FONT_NAME"
+        fc-cache -fv
+        rm -rf "${TEMP_DIR}"
+        printf "%b\n" "${GREEN}'$FONT_NAME' installed successfully.${RC}"
+    fi
+}
+
+installStarshipAndFzf() {
+    if command_exists starship; then
+        printf "%b\n" "${GREEN}Starship already installed${RC}"
+        return
+    fi
+
+    if ! curl -sSL https://starship.rs/install.sh | sh; then
+        printf "%b\n" "${RED}Something went wrong during starship install!${RC}"
+        exit 1
+    fi
+    if command_exists fzf; then
+        printf "%b\n" "${GREEN}Fzf already installed${RC}"
+    else
+        git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+        "$ESCALATION_TOOL" ~/.fzf/install
+    fi
+}
+
+installZoxide() {
+    if command_exists zoxide; then
+        printf "%b\n" "${GREEN}Zoxide already installed${RC}"
+        return
+    fi
+
+    if ! curl -sSL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh; then
+        printf "%b\n" "${RED}Something went wrong during zoxide install!${RC}"
+        exit 1
+    fi
+}
+
+linkConfig() {
+    OLD_BASHRC="$HOME/.bashrc"
+    if [ -e "$OLD_BASHRC" ] && [ ! -e "$HOME/.bashrc.bak" ]; then
+        printf "%b\n" "${YELLOW}Moving old bash config file to $HOME/.bashrc.bak${RC}"
+        if ! mv "$OLD_BASHRC" "$HOME/.bashrc.bak"; then
+            printf "%b\n" "${RED}Can't move the old bash config file!${RC}"
+            exit 1
+        fi
+    fi
+
+    printf "%b\n" "${YELLOW}Linking new bash config file...${RC}"
+    ln -svf "$gitpath/.bashrc" "$HOME/.bashrc" || {
+        printf "%b\n" "${RED}Failed to create symbolic link for .bashrc${RC}"
+        exit 1
+    }
+    ln -svf "$gitpath/starship.toml" "$HOME/.config/starship.toml" || {
+        printf "%b\n" "${RED}Failed to create symbolic link for starship.toml${RC}"
+        exit 1
+    }
+    printf "%b\n" "${GREEN}Done! restart your shell to see the changes.${RC}"
+}
+
+checkEnv
+checkEscalationTool
+installDepend
+cloneMyBash
+installFont
+installStarshipAndFzf
+installZoxide
+linkConfig
