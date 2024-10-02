dfc37322f89ad4038474aed56c6f331096567de2
Adam Perkowski <adas1per@protonmail.com>
2024-10-02 22:02:28 +0200

    dir backup

--- core/tabs/applications-setup/grub-theme.sh
+++ core/tabs/applications-setup/grub-theme.sh
@@ -4,7 +4,11 @@
 
 themeinstall(){
     mkdir -p "$HOME/.local/share"
-    cd "$HOME/.local/share" && git clone "https://github.com/ChrisTitusTech/Top-5-Bootloader-Themes"
+    cd "$HOME/.local/share"
+    if [ -d 'Top-5-Bootloader-Themes' ]; then
+        mv 'Top-5-Bootloader-Themes' 'Top-5-Bootloader-Themes.bak'
+    fi
+    git clone "https://github.com/ChrisTitusTech/Top-5-Bootloader-Themes"
     cd "$HOME/.local/share/Top-5-Bootloader-Themes"
     "$ESCALATION_TOOL" ./install.sh
 }
