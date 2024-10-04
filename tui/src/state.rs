919b55209ef13670817980f1041ecf4bc43bb352
nyx <nnyyxxxx@users.noreply.github.com>
2024-10-04 00:45:16 -0400

    obey our god lints

--- tui/src/state.rs
+++ tui/src/state.rs
@@ -667,7 +667,8 @@ impl AppState {
     fn enable_description(&mut self) {
         if let Some(command_description) = self.get_selected_description() {
             if !command_description.is_empty() {
-                let description = FloatingText::new(command_description, FloatingTextMode::Description);
+                let description =
+                    FloatingText::new(command_description, FloatingTextMode::Description);
                 self.spawn_float(description, 80, 80);
             }
         }
