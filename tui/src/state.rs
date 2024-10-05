5d522a3300483eb8337f11a020fe3c9875d89c18
JEEVITHA KANNAN K S <ksjeevithakannan123@gmail.com>
2024-10-05 23:25:43 +0530

    Quit with CTRL-C when in search

--- tui/src/state.rs
+++ tui/src/state.rs
@@ -411,11 +411,15 @@ impl AppState {
         // This should be defined first to allow closing
         // the application even when not drawable ( If terminal is small )
         // Exit on 'q' or 'Ctrl-c' input
-        if matches!(
+        if (matches!(
             self.focus,
-            Focus::TabList | Focus::List | Focus::ConfirmationPrompt(_)
-        ) && (key.code == KeyCode::Char('q')
-            || key.modifiers.contains(KeyModifiers::CONTROL) && key.code == KeyCode::Char('c'))
+            Focus::TabList | Focus::List | Focus::ConfirmationPrompt(_) | Focus::Search
+        ) && key.modifiers.contains(KeyModifiers::CONTROL)
+            && key.code == KeyCode::Char('c'))
+            || (matches!(
+                self.focus,
+                Focus::TabList | Focus::List | Focus::ConfirmationPrompt(_)
+            ) && key.code == KeyCode::Char('q'))
         {
             return false;
         }
