46fd0e87fc3ae5317677133f8a1e14a304b9e44a
Albert de Palo Hardvendel <71041549+Albert-LGTM@users.noreply.github.com>
2024-10-10 15:55:50 +0200

    Update core/tabs/applications-setup/podman-setup.sh
    Co-authored-by: nyx <nnyyxxxx@protonmail.com>

--- core/tabs/applications-setup/podman-setup.sh
+++ core/tabs/applications-setup/podman-setup.sh
@@ -2,31 +2,27 @@
 
 . ../common-script.sh
 
-install_podman() {
-    printf "%b\n" "${YELLOW}Installing Podman...${RC}"
-    case "$PACKAGER" in
-        apt-get|nala)
-            "$ESCALATION_TOOL" "$PACKAGER" install -y podman
-            ;;
-        zypper)
-            "$ESCALATION_TOOL" "$PACKAGER" --non-interactive install podman
-            ;;
-        pacman)
-            "$ESCALATION_TOOL" "$PACKAGER" -S --noconfirm --needed podman
-            ;;
-        dnf)
-            "$ESCALATION_TOOL" "$PACKAGER" install -y podman
-            ;;
-        *)
-            printf "%b\n" "${RED}Unsupported package manager: ""$PACKAGER""${RC}"
-            exit 1
-            ;;
-    esac
-}
-
-install_components() {
+installPodman() {
     if ! command_exists podman; then
-        install_podman
+        printf "%b\n" "${YELLOW}Installing Podman...${RC}"
+        case "$PACKAGER" in
+            apt-get|nala)
+                "$ESCALATION_TOOL" "$PACKAGER" install -y podman
+                ;;
+            zypper)
+                "$ESCALATION_TOOL" "$PACKAGER" --non-interactive install podman
+                ;;
+            pacman)
+                "$ESCALATION_TOOL" "$PACKAGER" -S --noconfirm --needed podman
+                ;;
+            dnf)
+                "$ESCALATION_TOOL" "$PACKAGER" install -y podman
+                ;;
+            *)
+                printf "%b\n" "${RED}Unsupported package manager: ""$PACKAGER""${RC}"
+                exit 1
+                ;;
+        esac
     else
         printf "%b\n" "${GREEN}Podman is already installed.${RC}"
     fi
