9611dd378651e5cfc3d41bf77ef059de3ac3591f
Albert de Palo Hardvendel <71041549+Albert-LGTM@users.noreply.github.com>
2024-10-10 15:56:09 +0200

    Update core/tabs/applications-setup/podman-compose-setup.sh
    Co-authored-by: nyx <nnyyxxxx@protonmail.com>

--- core/tabs/applications-setup/podman-compose-setup.sh
+++ core/tabs/applications-setup/podman-compose-setup.sh
@@ -2,31 +2,27 @@
 
 . ../common-script.sh
 
-install_podman_compose() {
-    printf "%b\n" "${YELLOW}Installing Podman Compose...${RC}"
-    case "$PACKAGER" in
-        apt-get|nala)
-            "$ESCALATION_TOOL" "$PACKAGER" install -y podman-compose
-            ;;
-        zypper)
-            "$ESCALATION_TOOL" "$PACKAGER" --non-interactive install podman-compose
-            ;;
-        pacman)
-            "$ESCALATION_TOOL" "$PACKAGER" -S --noconfirm --needed podman-compose
-            ;;
-        dnf)
-            "$ESCALATION_TOOL" "$PACKAGER" install -y podman-compose
-            ;;
-        *)
-            printf "%b\n" "${RED}Unsupported package manager: ""$PACKAGER""${RC}"
-            exit 1
-            ;;
-    esac
-}
-
-install_components() {
-    if ! command_exists podman-compose || ! command_exists podman compose version; then
-        install_podman_compose
+installPodmanCompose() {
+    if ! command_exists podman-compose; then
+        printf "%b\n" "${YELLOW}Installing Podman Compose...${RC}"
+        case "$PACKAGER" in
+            apt-get|nala)
+                "$ESCALATION_TOOL" "$PACKAGER" install -y podman-compose
+                ;;
+            zypper)
+                "$ESCALATION_TOOL" "$PACKAGER" --non-interactive install podman-compose
+                ;;
+            pacman)
+                "$ESCALATION_TOOL" "$PACKAGER" -S --noconfirm --needed podman-compose
+                ;;
+            dnf)
+                "$ESCALATION_TOOL" "$PACKAGER" install -y podman-compose
+                ;;
+            *)
+                printf "%b\n" "${RED}Unsupported package manager: ""$PACKAGER""${RC}"
+                exit 1
+                ;;
+        esac
     else
         printf "%b\n" "${GREEN}Podman Compose is already installed.${RC}"
     fi
