d8e6d67ce2292cfbc6e3ecec255497c93dc9aea4
pika <67532734+pik4li@users.noreply.github.com>
2024-10-18 17:42:37 +0200

    Update system-cleanup.sh
    Removed bug with autoclean for nala, becouse nala doesn't have the autoclean command

--- core/tabs/system-setup/system-cleanup.sh
+++ core/tabs/system-setup/system-cleanup.sh
@@ -5,13 +5,20 @@
 cleanup_system() {
     printf "%b\n" "${YELLOW}Performing system cleanup...${RC}"
     case "$PACKAGER" in
-        apt-get|nala)
+        apt-get)
             "$ESCALATION_TOOL" "$PACKAGER" clean
             "$ESCALATION_TOOL" "$PACKAGER" autoremove -y
             "$ESCALATION_TOOL" "$PACKAGER" autoclean
             "$ESCALATION_TOOL" du -h /var/cache/apt
             "$ESCALATION_TOOL" "$PACKAGER" clean
             ;;
+        nala)
+            "$ESCALATION_TOOL" "$PACKAGER" clean
+            "$ESCALATION_TOOL" "$PACKAGER" autoremove -y
+            "$ESCALATION_TOOL" apt-get autoclean
+            "$ESCALATION_TOOL" du -h /var/cache/apt
+            "$ESCALATION_TOOL" "$PACKAGER" clean
+            ;;            
         zypper)
             "$ESCALATION_TOOL" "$PACKAGER" clean -a
             "$ESCALATION_TOOL" "$PACKAGER" tidy
