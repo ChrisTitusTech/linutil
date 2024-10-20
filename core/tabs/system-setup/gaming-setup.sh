df008d63e326ce5e08886add0426bf60196990c5
Jeevitha Kannan K S <ksjeevithakannan123@gmail.com>
2024-10-20 21:33:35 +0530

    fix: pop-os gaming dependency issue

--- core/tabs/system-setup/gaming-setup.sh
+++ core/tabs/system-setup/gaming-setup.sh
@@ -3,8 +3,7 @@
 . ../common-script.sh
 
 installDepend() {
-    # Check for dependencies
-    DEPENDENCIES='wine dbus'
+    DEPENDENCIES='wine dbus git'
     printf "%b\n" "${YELLOW}Installing dependencies...${RC}"
     case "$PACKAGER" in
         pacman)
@@ -26,12 +25,12 @@ installDepend() {
             $AUR_HELPER -S --needed --noconfirm $DEPENDENCIES $DISTRO_DEPS
             ;;
         apt-get|nala)
-            DISTRO_DEPS="libasound2 libsdl2-2.0-0 wine64 wine32"
+            DISTRO_DEPS="software-properties-common libasound2 libsdl2-2.0-0 wine64 wine32"
 
-            "$ESCALATION_TOOL" "$PACKAGER" update
             "$ESCALATION_TOOL" dpkg --add-architecture i386
-            "$ESCALATION_TOOL" "$PACKAGER" install -y software-properties-common
-            "$ESCALATION_TOOL" apt-add-repository contrib -y
+            if [ "$DTYPE" != "pop" ]; then
+                "$ESCALATION_TOOL" apt-add-repository contrib -y
+            fi
             "$ESCALATION_TOOL" "$PACKAGER" update
             "$ESCALATION_TOOL" "$PACKAGER" install -y $DEPENDENCIES $DISTRO_DEPS
             ;;
@@ -50,7 +49,8 @@ installDepend() {
             "$ESCALATION_TOOL" "$PACKAGER" -n install $DEPENDENCIES
             ;;
         *)
-            "$ESCALATION_TOOL" "$PACKAGER" install -y $DEPENDENCIES
+            printf "%b\n" "${RED}Unsupported package manager ${PACKAGER}${RC}"
+            exit 1
             ;;
     esac
 }
@@ -90,11 +90,12 @@ installAdditionalDepend() {
             "$ESCALATION_TOOL" "$PACKAGER" install -y $DISTRO_DEPS
             ;;
         zypper)
-            # Flatpak
             DISTRO_DEPS='lutris'
             "$ESCALATION_TOOL" "$PACKAGER" -n install $DISTRO_DEPS
             ;;
         *)
+            printf "%b\n" "${RED}Unsupported package manager ${PACKAGER}${RC}"
+            exit 1
             ;;
     esac
 }
