09aea49b2c031c1b81f3c7fbc6e4f4a60433ec00
Harsh Vyapari <harshv5094@gmail.com>
2024-10-20 17:57:44 +0530

    Update core/tabs/applications-setup/communication-apps/whatsapp-setup.sh
    Co-authored-by: JEEVITHA KANNAN K S <ksjeevithakannan123@gmail.com>

--- core/tabs/applications-setup/communication-apps/whatsapp-setup.sh
+++ core/tabs/applications-setup/communication-apps/whatsapp-setup.sh
@@ -4,26 +4,20 @@
 
 installWhatsapp() {
   printf "%b\n" "${YELLOW}The app we are installing is Zap-Zap, an Open Source Desktop Client for Linux${RC}"
-  case "$PACKAGER" in
-  pacman)
-    if ! command_exists zapzap; then
-      printf "%b\n" "${YELLOW}Installing Whatsapp(Zap-Zap by rtosta) AUR PACKAGE${RC}"
-      "$AUR_HELPER" -S --needed --noconfirm zapzap
-      printf "%b\n" "${GREEN}WhatsApp(Zap-Zap by rtosta) is installed. Search ZapZap to start WhatsApp${RC}"
-      return
-    else
-      printf "%b\n" "${GREEN}Whatsapp(Zap-Zap by rtosta) is already installed${RC}"
-      return
-    fi
-    ;;
-  *)
-    . ../setup-flatpak.sh
-    printf "%b\n" "${YELLOW} Installing Whatsapp(Zap-Zap by rtosta) Flatpak ${RC}"
-    flatpak install flathub com.rtosta.zapzap
-    printf "%b\n" "${GREEN}WhatsApp(Zap-Zap by rtosta) is installed. Search ZapZap to start WhatsApp${RC}"
-    return
-    ;;
-  esac
+  if ! command_exists com.rtosta.zapzap && ! command_exists zapzap; then
+    printf "%b\n" "${YELLOW}Installing Whatsapp(Zap-Zap by rtosta)${RC}"
+    case "$PACKAGER" in
+      pacman)
+        "$AUR_HELPER" -S --needed --noconfirm zapzap
+        ;;
+      *)
+        . ../setup-flatpak.sh
+        flatpak install flathub com.rtosta.zapzap
+        ;;
+    esac
+  else
+    printf "%b\n" "${GREEN}WhatsApp(Zap-Zap by rtosta) is already installed.${RC}"
+  fi
 }
 
 checkEnv
