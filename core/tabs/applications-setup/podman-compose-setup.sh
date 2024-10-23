49df3f6e827b4a2fe6ba50fe890e4ed25edd2f1f
Albert de Palo Hardvendel <71041549+Albert-LGTM@users.noreply.github.com>
2024-10-23 09:51:45 +0200

    Update core/tabs/applications-setup/podman-compose-setup.sh
    Removed redundant quotes
    
    Co-authored-by: JEEVITHA KANNAN K S <ksjeevithakannan123@gmail.com>

--- core/tabs/applications-setup/podman-compose-setup.sh
+++ core/tabs/applications-setup/podman-compose-setup.sh
@@ -19,7 +19,7 @@ installPodmanCompose() {
                 "$ESCALATION_TOOL" "$PACKAGER" install -y podman-compose
                 ;;
             *)
-                printf "%b\n" "${RED}Unsupported package manager: ""$PACKAGER""${RC}"
+                printf "%b\n" "${RED}Unsupported package manager: ${PACKAGER}${RC}"
                 exit 1
                 ;;
         esac
