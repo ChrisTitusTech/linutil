b51af97916c62322278a224ac8fe23073999f47f
pika <alex@team-pieck.de>
2024-10-27 17:23:09 +0100

    Made it an option, to clone and use Titus's dotfiles

--- core/tabs/applications-setup/Developer-tools/neovim-setup.sh
+++ core/tabs/applications-setup/Developer-tools/neovim-setup.sh
@@ -15,25 +15,25 @@ cloneNeovim() {
 
 installNeovim() {
     if ! command_exists neovim ripgrep git fzf; then
-    printf "%b\n" "${YELLOW}Installing Neovim...${RC}"
-    case "$PACKAGER" in
+        printf "%b\n" "${YELLOW}Installing Neovim...${RC}"
+        case "$PACKAGER" in
         pacman)
             "$ESCALATION_TOOL" "$PACKAGER" -S --needed --noconfirm neovim ripgrep fzf python-virtualenv luarocks go shellcheck git
             ;;
-        apt-get|nala)
+        apt-get | nala)
             "$ESCALATION_TOOL" "$PACKAGER" install -y ripgrep fd-find python3-venv luarocks golang-go shellcheck git
             curl -sSLo /tmp/nvim.appimage https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
             chmod u+x /tmp/nvim.appimage
             "$ESCALATION_TOOL" mv /tmp/nvim.appimage /usr/local/bin/nvim
             ;;
-        dnf|zypper)
+        dnf | zypper)
             "$ESCALATION_TOOL" "$PACKAGER" install -y neovim ripgrep fzf python3-virtualenv luarocks golang ShellCheck git
             ;;
         *)
             printf "%b\n" "${RED}Unsupported package manager: ""$PACKAGER""${RC}"
             exit 1
             ;;
-    esac
+        esac
     fi
 }
 
@@ -54,6 +54,20 @@ linkNeovimConfig() {
 checkEnv
 checkEscalationTool
 installNeovim
-cloneNeovim
-backupNeovimConfig
-linkNeovimConfig
+printf "%b\n" "${GREEN}Do you also want to install the TitusDotfiles for neovim? (Y/n): ${RC}"
+read -r _titusDots
+case "$_titusDots" in
+Y | y)
+    cloneNeovim
+    backupNeovimConfig
+    linkNeovimConfig
+    ;;
+N | n)
+    printf "%b\n" "${YELLOW}Installation complete.."
+    ;;
+*)
+    cloneNeovim
+    backupNeovimConfig
+    linkNeovimConfig
+    ;;
+esac
