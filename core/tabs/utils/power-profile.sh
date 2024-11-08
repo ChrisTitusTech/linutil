95cfbe8920b2675a148ce6c23d13d78bb8771afb
Infinite State <165737308+infstate@users.noreply.github.com>
2024-11-08 07:06:47 -0800

    fix: Autocpu-freq script not detecting battery on laptops (#877)
    * Add refined check
    
    * remove an unneccessary comment
    
    Co-authored-by: nyx <nnyyxxxx@protonmail.com>
    
    ---------
    
    Co-authored-by: Adam Perkowski <adas1per@protonmail.com>
    Co-authored-by: nyx <nnyyxxxx@protonmail.com>

--- core/tabs/utils/power-profile.sh
+++ core/tabs/utils/power-profile.sh
@@ -48,7 +48,7 @@ configureAutoCpufreq() {
 
     if command_exists auto-cpufreq; then
         # Check if the system has a battery to determine if it's a laptop
-        if [ -d /sys/class/power_supply/BAT0 ]; then
+        if ls /sys/class/power_supply/BAT* >/dev/null 2>&1; then
             printf "%b\n" "${GREEN}System detected as laptop. Updating auto-cpufreq for laptop...${RC}"
             "$ESCALATION_TOOL" auto-cpufreq --force powersave
         else
