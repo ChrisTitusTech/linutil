a772566456f9abe5327fb91c77ddb8656e4f48db
Jaredy899 <76603258+Jaredy899@users.noreply.github.com>
2024-12-24 09:02:47 -0500

    Solus packager

--- core/tabs/common-script.sh
+++ core/tabs/common-script.sh
@@ -182,7 +182,7 @@ checkEnv() {
     checkArch
     checkEscalationTool
     checkCommandRequirements "curl groups $ESCALATION_TOOL"
-    checkPackageManager 'nala apt-get dnf pacman zypper apk xbps-install'
+    checkPackageManager 'nala apt-get dnf pacman zypper apk xbps-install eopkg'
     checkCurrentDirectoryWritable
     checkSuperUser
     checkDistro
