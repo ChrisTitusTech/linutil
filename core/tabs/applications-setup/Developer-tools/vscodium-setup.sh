28a626957e5b4cc53f70cf6b31a04b031bebc69f
Gerjan Lleshaj <64326282+notaprodev@users.noreply.github.com>
2024-12-24 19:01:42 +0100

    fix issue: apt vscodium install on debian (#870)

--- core/tabs/applications-setup/Developer-tools/vscodium-setup.sh
+++ core/tabs/applications-setup/Developer-tools/vscodium-setup.sh
@@ -8,7 +8,7 @@ installVsCodium() {
         case "$PACKAGER" in
             apt-get|nala)
                 curl -fsSL https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | gpg --dearmor | "$ESCALATION_TOOL" dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
-                echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://download.vscodium.com/debs vscodium main' | "$ESCALATION_TOOL" tee /etc/apt/sources.list.d/vscodium.list
+                echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://download.vscodium.com/debs vscodium main' | "$ESCALATION_TOOL" tee /etc/apt/sources.list.d/vscodium.list
                 "$ESCALATION_TOOL" "$PACKAGER" update
                 "$ESCALATION_TOOL" "$PACKAGER" install -y codium
                 ;;
