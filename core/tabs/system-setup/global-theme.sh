5d2ee0e21d3a05a3aa26cfe87c237fd9ca21676d
Jaredy899 <76603258+Jaredy899@users.noreply.github.com>
2025-01-02 23:26:58 -0500

    Update global-theme.sh

--- core/tabs/system-setup/global-theme.sh
+++ core/tabs/system-setup/global-theme.sh
@@ -5,23 +5,17 @@
 install_theme_tools() {
     printf "%b\n" "${YELLOW}Installing theme tools (qt6ct and kvantum)...${RC}"
     case "$PACKAGER" in
-        apt-get|nala)
+        apt-get|nala|dnf|eopkg)
             "$ESCALATION_TOOL" "$PACKAGER" install -y qt6ct kvantum
             ;;
         zypper)
             "$ESCALATION_TOOL" "$PACKAGER" --non-interactive install qt6ct kvantum
-            ;;
-        dnf)
-            "$ESCALATION_TOOL" "$PACKAGER" install -y qt6ct kvantum
             ;;
         pacman)
             "$ESCALATION_TOOL" "$PACKAGER" -S --needed --noconfirm qt6ct kvantum
             ;;
         xbps-install)
             "$ESCALATION_TOOL" "$PACKAGER" -y qt6ct kvantum
-            ;;
-        eopkg)
-            "$ESCALATION_TOOL" "$PACKAGER" -y install qt6ct kvantum
             ;;
         *)
             printf "%b\n" "${RED}Unsupported package manager: ""$PACKAGER""${RC}"
