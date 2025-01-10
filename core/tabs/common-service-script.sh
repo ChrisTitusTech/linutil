8aeffe9f08990e50ca10d0b8936961bfe3449337
Jaredy899 <76603258+Jaredy899@users.noreply.github.com>
2025-01-10 15:30:05 -0500

    Update common-service-script.sh

--- core/tabs/common-service-script.sh
+++ core/tabs/common-service-script.sh
@@ -52,7 +52,6 @@ enableService() {
             "$ESCALATION_TOOL" rc-update add "$1"
             ;;
         runit)
-            "$ESCALATION_TOOL" mkdir -p "/run/runit/supervise.$1"
             "$ESCALATION_TOOL" ln -sf "/etc/sv/$1" "/var/service/"
             sleep 5
             ;;
