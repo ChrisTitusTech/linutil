9c71edf06122561cf077b014271a988dfff978e6
Jaredy899 <76603258+Jaredy899@users.noreply.github.com>
2025-01-11 09:25:40 -0500

    Update core/tabs/common-service-script.sh
    Found it faster than me. Thanks
    
    Co-authored-by: Adam Perkowski <adas1per@protonmail.com>

--- core/tabs/common-service-script.sh
+++ core/tabs/common-service-script.sh
@@ -78,7 +78,7 @@ startAndEnableService() {
         systemctl)
             "$ESCALATION_TOOL" "$INIT_MANAGER" enable --now "$1"
             ;;
-        rc-service|runit)
+        rc-service | runit)
             enableService "$1"
             startService "$1"
             ;;
