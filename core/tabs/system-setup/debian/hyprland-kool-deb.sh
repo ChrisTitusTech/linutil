7fa4d2dcf0f1abecdeea2033509f0f92335d9ad7
Chris Titus <contact@christitus.com>
2025-02-05 11:07:49 -0600

    Update core/tabs/system-setup/debian/hyprland-kool-deb.sh
    Co-authored-by: nyx <nnyyxxxx@protonmail.com>

--- core/tabs/system-setup/debian/hyprland-kool-deb.sh
+++ core/tabs/system-setup/debian/hyprland-kool-deb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Hyprland JaKooLit"
-git clone --depth=1 https://github.com/JaKooLit/Debian-Hyprland.git ~/Debian-Hyprland
+git clone --depth=1 https://github.com/JaKooLit/Debian-Hyprland.git "$HOME/Debian-Hyprland" || { printf "%b\n" "${RED}Failed to clone Jakoolits Debian-Hyprland repo${RC}"; exit 1; }
 cd "$HOME/Debian-Hyprland"
 chmod +x install.sh
 ./install.sh
