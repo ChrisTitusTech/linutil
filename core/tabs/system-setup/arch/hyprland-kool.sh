ad05a0d4680d2f6b26102017919f752270469e0b
Chris Titus <contact@christitus.com>
2025-02-05 11:07:36 -0600

    Update core/tabs/system-setup/arch/hyprland-kool.sh
    Co-authored-by: nyx <nnyyxxxx@protonmail.com>

--- core/tabs/system-setup/arch/hyprland-kool.sh
+++ core/tabs/system-setup/arch/hyprland-kool.sh
@@ -7,7 +7,7 @@ if ! pacman -Q base-devel &>/dev/null; then
     sudo pacman -S --noconfirm base-devel
 fi
 
-git clone --depth=1 https://github.com/JaKooLit/Arch-Hyprland.git ~/Arch-Hyprland
+git clone --depth=1 https://github.com/JaKooLit/Arch-Hyprland.git "$HOME/Arch-Hyprland" || { printf "%b\n" "${RED}Failed to clone Jakoolits Arch-Hyprland repo${RC}"; exit 1; }
 cd "$HOME/Arch-Hyprland"
 chmod +x install.sh
 ./install.sh
