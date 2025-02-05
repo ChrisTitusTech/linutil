e5fdbfff7d1893b531d0120bdee814f5798124ec
Chris Titus <contact@christitus.com>
2025-02-05 11:08:29 -0600

    Update core/tabs/system-setup/arch/hyprland-kool.sh
    Co-authored-by: nyx <nnyyxxxx@protonmail.com>

--- core/tabs/system-setup/arch/hyprland-kool.sh
+++ core/tabs/system-setup/arch/hyprland-kool.sh
@@ -6,7 +6,7 @@ printf "%b\n" "${YELLOW}Starting Hyprland JaKooLit installation${RC}"
 
 if ! pacman -Q base-devel &>/dev/null; then
     printf "%b\n" "${YELLOW}Installing base-devel...${RC}"
-    sudo pacman -S --noconfirm base-devel
+    "$ESCALATION_TOOL" pacman -S --noconfirm base-devel
 fi
 
 git clone --depth=1 https://github.com/JaKooLit/Arch-Hyprland.git "$HOME/Arch-Hyprland" || { printf "%b\n" "${RED}Failed to clone Jakoolits Arch-Hyprland repo${RC}"; exit 1; }
