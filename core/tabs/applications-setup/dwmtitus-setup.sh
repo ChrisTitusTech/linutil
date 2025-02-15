599497f7025a38c116e0a76dc463caa5f10a5dc1
Jaredy899 <76603258+Jaredy899@users.noreply.github.com>
2025-02-14 23:23:04 -0500

    Update dwmtitus-setup.sh

--- core/tabs/applications-setup/dwmtitus-setup.sh
+++ core/tabs/applications-setup/dwmtitus-setup.sh
@@ -12,7 +12,7 @@ setupDWM() {
             "$ESCALATION_TOOL" "$PACKAGER" install -y build-essential libx11-dev libxinerama-dev libxft-dev libimlib2-dev libx11-xcb-dev libfontconfig1 libx11-6 libxft2 libxinerama1 libxcb-res0-dev git unzip flameshot lxappearance feh mate-polkit
             ;;
         dnf)
-            "$ESCALATION_TOOL" "$PACKAGER" groupinstall -y "Development Tools"
+            "$ESCALATION_TOOL" "$PACKAGER" install -y "@development-tools" || "$ESCALATION_TOOL" "$PACKAGER" group install -y "Development Tools"
             "$ESCALATION_TOOL" "$PACKAGER" install -y libX11-devel libXinerama-devel libXft-devel imlib2-devel libxcb-devel unzip flameshot lxappearance feh mate-polkit # no need to include git here as it should be already installed via "Development Tools"
             ;;
         *)
