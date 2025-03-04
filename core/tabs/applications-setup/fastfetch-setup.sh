ac4c64116cb3d9932d2034f09f794553c56a60fa
Jaredy899 <76603258+Jaredy899@users.noreply.github.com>
2025-03-03 22:47:06 -0500

    fix formatting

--- core/tabs/applications-setup/fastfetch-setup.sh
+++ core/tabs/applications-setup/fastfetch-setup.sh
@@ -9,19 +9,19 @@ installFastfetch() {
         pacman)
             "$ESCALATION_TOOL" "$PACKAGER" -S --needed --noconfirm fastfetch
             ;;
-            apt-get | nala)
-                case "$ARCH" in
-                    x86_64)
-                        DEB_FILE="fastfetch-linux-amd64.deb"
-                        ;;
-                    aarch64)
-                        DEB_FILE="fastfetch-linux-aarch64.deb"
-                        ;;
-                esac
-                curl -sSLo "/tmp/fastfetch.deb" "https://github.com/fastfetch-cli/fastfetch/releases/latest/download/$DEB_FILE"
-                "$ESCALATION_TOOL" "$PACKAGER" install -y /tmp/fastfetch.deb
-                rm /tmp/fastfetch.deb
-                ;;
+        apt-get | nala)
+            case "$ARCH" in
+                x86_64)
+                    DEB_FILE="fastfetch-linux-amd64.deb"
+                    ;;
+                aarch64)
+                    DEB_FILE="fastfetch-linux-aarch64.deb"
+                    ;;
+            esac
+            curl -sSLo "/tmp/fastfetch.deb" "https://github.com/fastfetch-cli/fastfetch/releases/latest/download/$DEB_FILE"
+            "$ESCALATION_TOOL" "$PACKAGER" install -y /tmp/fastfetch.deb
+            rm /tmp/fastfetch.deb
+            ;;
         apk)
             "$ESCALATION_TOOL" "$PACKAGER" add fastfetch
             ;;
