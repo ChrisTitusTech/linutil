f38e90d101feb44121cb11a25c01f198d84dbc47
Jaredy899 <76603258+Jaredy899@users.noreply.github.com>
2025-03-11 19:41:29 -0400

    Prevent brute force

--- core/tabs/security/firewalld-baselines.sh
+++ core/tabs/security/firewalld-baselines.sh
@@ -11,6 +11,18 @@ configureFirewallD() {
     printf "%b\n" "${YELLOW}Allowing SSH service (FirewallD)${RC}"
     "$ESCALATION_TOOL" firewall-cmd --permanent --add-service=ssh
 
+    printf "%b\n" "${YELLOW}Implementing SSH brute force protection (FirewallD)${RC}"
+    "$ESCALATION_TOOL" firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT_direct 0 -p tcp --dport 22 \
+        -m state --state NEW -m recent --set
+    "$ESCALATION_TOOL" firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT_direct 1 -p tcp --dport 22 \
+        -m state --state NEW -m recent --update --seconds 30 --hitcount 6 \
+        -j REJECT --reject-with tcp-reset
+    "$ESCALATION_TOOL" firewall-cmd --permanent --direct --add-rule ipv6 filter INPUT_direct 0 -p tcp --dport 22 \
+        -m state --state NEW -m recent --set
+    "$ESCALATION_TOOL" firewall-cmd --permanent --direct --add-rule ipv6 filter INPUT_direct 1 -p tcp --dport 22 \
+        -m state --state NEW -m recent --update --seconds 30 --hitcount 6 \
+        -j REJECT --reject-with tcp-reset
+
     printf "%b\n" "${YELLOW}Allowing HTTP service (FirewallD)${RC}"
     "$ESCALATION_TOOL" firewall-cmd --permanent --add-service=http
 
