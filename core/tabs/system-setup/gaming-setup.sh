1f021cdd2d4f104ba4d15719c6158f41aaeb7159
Jared Cervantes <76603258+Jaredy899@users.noreply.github.com>
2025-06-20 16:26:53 -0400

    fix(gaming-setup): Fedora gaming fix (#1061)
    * correct rpm logic
    
    * Delete Fedora 40 logic
    
    Now that Fedora 40 is EOL, deleted install logic as I doubt few people will install gaming stuff on old fedora os.

--- core/tabs/system-setup/gaming-setup.sh
+++ core/tabs/system-setup/gaming-setup.sh
@@ -40,15 +40,11 @@ installDepend() {
             "$ESCALATION_TOOL" "$PACKAGER" install -y $DEPENDENCIES $DISTRO_DEPS
             ;;
         dnf)
-            if [ "$(rpm -E %fedora)" -le 41 ]; then
-                "$ESCALATION_TOOL" "$PACKAGER" install ffmpeg ffmpeg-libs -y
-                "$ESCALATION_TOOL" "$PACKAGER" install -y $DEPENDENCIES
-            else
-                printf "%b\n" "${CYAN}Fedora < 41 detected. Installing rpmfusion repos.${RC}"
-                "$ESCALATION_TOOL" "$PACKAGER" install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm -y
-                "$ESCALATION_TOOL" "$PACKAGER" config-manager --enable fedora-cisco-openh264 -y
-                "$ESCALATION_TOOL" "$PACKAGER" install -y $DEPENDENCIES
-            fi
+            printf "%b\n" "${CYAN}Installing rpmfusion repos.${RC}"
+            "$ESCALATION_TOOL" "$PACKAGER" install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm -y
+            "$ESCALATION_TOOL" "$PACKAGER" config-manager setopt --repo fedora-cisco-openh264 enabled=1
+    
+            "$ESCALATION_TOOL" "$PACKAGER" install -y $DEPENDENCIES
             ;;
         zypper)
             "$ESCALATION_TOOL" "$PACKAGER" -n install $DEPENDENCIES
