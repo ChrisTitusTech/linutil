d4549012a6c89c7f3b35bc8ef6438d8cc6cd795a
Jared Cervantes <jared@jaredcervantes.com>
2025-06-27 12:04:08 -0400

    smaller font

--- core/tabs/system-setup/terminus-tty.sh
+++ core/tabs/system-setup/terminus-tty.sh
@@ -43,18 +43,18 @@ SetTermiusFonts() {
         case "$DTYPE" in
             arch|fedora|void|solus|opensuse-*)
                 printf "%b\n" "${YELLOW}Updating FONT= line in /etc/vconsole.conf...${RC}"
-                "$ESCALATION_TOOL" sed -i 's/^FONT=.*/FONT=ter-v32b/' /etc/vconsole.conf
+                "$ESCALATION_TOOL" sed -i 's/^FONT=.*/FONT=ter-v18b/' /etc/vconsole.conf
                 if [ -z "$DISPLAY" ] && [ -z "$WAYLAND_DISPLAY" ]; then
-                   "$ESCALATION_TOOL" setfont -C /dev/tty1 ter-v32b
+                   "$ESCALATION_TOOL" setfont -C /dev/tty1 ter-v18b
                 fi
                 printf "%b\n" "${GREEN}Terminus font set for TTY.${RC}"
                 ;;
             alpine)
                 printf "%b\n" "${YELLOW}Updating console font configuration for Alpine...${RC}"
                 if [ -z "$DISPLAY" ] && [ -z "$WAYLAND_DISPLAY" ]; then
-                    "$ESCALATION_TOOL" setfont -C /dev/tty1 /usr/share/consolefonts/ter-v32b.psf.gz
+                    "$ESCALATION_TOOL" setfont -C /dev/tty1 /usr/share/consolefonts/ter-v18b.psf.gz
                 fi
-                echo 'consolefont="/usr/share/consolefonts/ter-v32b.psf.gz"' | "$ESCALATION_TOOL" tee /etc/conf.d/consolefont > /dev/null
+                echo 'consolefont="/usr/share/consolefonts/ter-v18b.psf.gz"' | "$ESCALATION_TOOL" tee /etc/conf.d/consolefont > /dev/null
                 "$ESCALATION_TOOL" rc-update add consolefont boot
                 printf "%b\n" "${GREEN}Terminus font set for TTY.${RC}"
                 ;;
@@ -62,12 +62,12 @@ SetTermiusFonts() {
                 printf "%b\n" "${YELLOW}Updating console-setup configuration...${RC}"
                 "$ESCALATION_TOOL" sed -i 's/^CODESET=.*/CODESET="guess"/' /etc/default/console-setup
                 "$ESCALATION_TOOL" sed -i 's/^FONTFACE=.*/FONTFACE="TerminusBold"/' /etc/default/console-setup
-                "$ESCALATION_TOOL" sed -i 's/^FONTSIZE=.*/FONTSIZE="16x32"/' /etc/default/console-setup
+                "$ESCALATION_TOOL" sed -i 's/^FONTSIZE=.*/FONTSIZE="10x18"/' /etc/default/console-setup
                 printf "%b\n" "${GREEN}Console-setup configuration updated for Terminus font.${RC}"
                 # Editing console-setup requires initramfs to be regenerated
                 "$ESCALATION_TOOL" update-initramfs -u
                 if [ -z "$DISPLAY" ] && [ -z "$WAYLAND_DISPLAY" ]; then                    
-                   "$ESCALATION_TOOL" setfont -C /dev/tty1 /usr/share/consolefonts/Uni3-TerminusBold32x16.psf.gz
+                   "$ESCALATION_TOOL" setfont -C /dev/tty1 /usr/share/consolefonts/Uni3-TerminusBold18x10.psf.gz
                 fi
                 printf "%b\n" "${GREEN}Terminus font has been set for TTY.${RC}"
                 ;;
