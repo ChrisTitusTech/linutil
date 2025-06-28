21fe9396280e412a44835b191201d777bdf4b7c6
Alexander Brown <itman91@gmail.com>
2025-06-28 16:25:45 +0100

    Added Install HP Printer Drivers script

---
+++ core/tabs/utils/printers/install-hp-printer-drivers.sh
@@ -0,0 +1,32 @@
+#!/bin/sh -e
+
+. ../../common-script.sh
+. ./install-cups.sh
+
+installHpPrinterDriver() {
+    clear
+
+    case "$PACKAGER" in
+    apt-get|nala)
+        "$ESCALATION_TOOL" "$PACKAGER" install -y hplip
+        ;;
+    dnf|eopkg)
+        "$ESCALATION_TOOL" "$PACKAGER" install -y hplip
+        ;;
+    pacman)
+        "$ESCALATION_TOOL" -S --noconfirm hplip-lite
+        ;;
+    xbps-install) 
+        "$ESCALATION_TOOL" "$PACKAGER" -Sy hplip
+        ;;
+    *)
+        printf "%b\n" "${RED}Unsupported package manager ${PACKAGER}${RC}"
+        exit 1
+        ;;
+    esac
+}
+
+checkEnv
+checkEscalationTool
+installCUPS
+installHpPrinterDriver
