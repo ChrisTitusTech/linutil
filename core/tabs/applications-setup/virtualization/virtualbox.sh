fe8756b760abf97a8a2ea6b90a56e47dc3b065a8
Nick Amato <namato@deloitte.com>
2025-08-22 15:34:43 -0400

    changed line 10 in virtualbox.sh to meet requirements

--- core/tabs/applications-setup/virtualization/virtualbox.sh
+++ core/tabs/applications-setup/virtualization/virtualbox.sh
@@ -7,7 +7,7 @@ installVirtualBox() {
     case "$PACKAGER" in
         apt-get|nala)
         	wget -O- https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo gpg --dearmor --yes --output /usr/share/keyrings/oracle-virtualbox-2016.gpg
-        	"$ESCALATION_TOOL" sh -c "echo "Types: deb\nURIs: http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs)\nSuites: $(lsb_release -cs 2>/dev/null)\nComponents: contrib\nArchitectures: $ARCH\nSigned-By: /usr/share/keyrings/oracle-virtualbox-2016.gpg\n" > /etc/apt/sources.list.d/virtualbox.sources"
+        	"$ESCALATION_TOOL" sh -c "echo "Types: deb\nURIs: http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs)\nSuites: $(lsb_release -cs 2>/dev/null)\nComponents: contrib\nArchitectures: "${ARCH}"\nSigned-By: /usr/share/keyrings/oracle-virtualbox-2016.gpg\n" > /etc/apt/sources.list.d/virtualbox.sources"
     		"$ESCALATION_TOOL" "$PACKAGER" update
             "$ESCALATION_TOOL" "$PACKAGER" -y install virtualbox-7.1
 
