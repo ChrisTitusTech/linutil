67f8e5c607f36838d3afa3b90cf640cc8e73d5ec
titus <contact@christitus.com>
2025-08-26 16:01:23 -0500

    f76 custom ini install

---
+++ core/tabs/gaming/fallout-76/fallout76settings.sh
@@ -0,0 +1,111 @@
+#!/bin/sh -e
+
+. ../../common-script.sh
+
+# Check for required commands
+for cmd in find curl unzip stty; do
+    if ! command_exists "$cmd"; then
+        printf "%b\n" "${RED}$cmd is not installed.${RC}"
+        exit 1
+    fi
+done
+
+# Search for possible Fallout compatdata directory structures
+printf "%b\n" "${YELLOW}Searching for Fallout 76 compatdata settings in document folders...${RC}"
+possible_paths=$(find "$HOME" -type d -path "*/compatdata/1151340/pfx/drive_c/users/steamuser/Documents/My Games/Fallout 76" 2>/dev/null)
+
+if [ -z "$possible_paths" ]; then
+    printf "%b\n" "${RED}No folders found.${RC}"
+    exit 1
+fi
+
+# Display possible paths and allow selection
+printf "%b\n" "Possible folder locations:"
+paths_string=""
+i=0
+IFS='
+'
+for path in $possible_paths; do
+    paths_string="$paths_string|$path"
+    i=$((i + 1))
+done
+IFS=' '
+paths_string="${paths_string#|}"
+total=$i
+selected=0
+
+print_menu() {
+    clear
+    max_display=$((total < 10 ? total : 10))
+    start=$((selected - max_display / 2))
+    if [ $start -lt 0 ]; then start=0; fi
+    if [ $((start + max_display)) -gt $total ]; then start=$((total - max_display)); fi
+    if [ $start -lt 0 ]; then start=0; fi
+    
+    printf "%b\n" "Please select the installation path:"
+    i=0
+    echo "$paths_string" | tr '|' '\n' | while IFS= read -r path; do
+        if [ $i -ge $start ] && [ $i -lt $((start + max_display)) ]; then
+            if [ $i -eq $selected ]; then
+                printf "> %s\n" "$path"
+            else
+                printf "  %s\n" "$path"
+            fi
+        fi
+        i=$((i + 1))
+    done
+}
+
+select_path() {
+    last_selected=-1
+
+    while true; do
+        if [ $last_selected -ne $selected ]; then
+            print_menu
+            last_selected=$selected
+        fi
+
+        stty -echo
+        key=$(dd bs=1 count=1 2>/dev/null)
+        stty echo
+
+        case "$key" in
+            "$(printf '\033')A" | k)
+                if [ $selected -gt 0 ]; then
+                    selected=$((selected - 1))
+                fi
+                ;;
+            "$(printf '\033')B" | j)
+                if [ $selected -lt $((total - 1)) ]; then
+                    selected=$((selected + 1))
+                fi
+                ;;
+            '')
+                game_path=$(echo "$paths_string" | cut -d '|' -f $((selected + 1)))
+                break
+                ;;
+        esac
+    done
+
+    clear
+}
+
+# Use the select_path function
+select_path
+
+# Validate the path
+if [ ! -d "$game_path" ]; then
+    printf "%b\n" "${RED}The specified path does not exist.${RC}"
+    exit 1
+fi
+
+# Download the latest release
+printf "%b\n" "Downloading the latest custom settings..."
+if ! curl -sSLo /tmp/Fallout76Custom.ini https://github.com/ChrisTitusTech/fallout76-configs/releases/latest/download/Fallout76Custom.ini; then
+    printf "%b\n" "${RED}Failed to download.${RC}"
+    exit 1
+fi
+
+mv /tmp/Fallout76Custom.ini "$game_path/Fallout76Custom.ini"
+
+printf "%b\n" "${GREEN}Settings installed successfully.${RC}"
