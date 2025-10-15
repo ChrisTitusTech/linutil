f3f27c4d81320b83098708f4526f8859baa027b8
floofywoofles <k.knapp.jr4@protonmail.com>
2025-10-15 14:17:28 -0400

    Added cursor to tab_data

---
+++ core/tabs/applications-setup/developer-tools/cursor.sh
@@ -0,0 +1,41 @@
+#!/bin/sh -e
+
+. ../../common-script.sh
+
+installCursor() {
+    if ! command_exists cursor; then
+        printf "%b\n" "${YELLOW}Installing Cursor...${RC}"
+        case "$PACKAGER" in
+            apt-get|nala)
+                TEMP_DEB="$(mktemp)"
+                wget -O "$TEMP_DEB" "https://api2.cursor.sh/updates/download/golden/linux-x64-deb/cursor/1.7"
+
+                "$ESCALATION_TOOL" "$PACKAGER" update
+                "$ESCALATION_TOOL" "$PACKAGER" install -y $TEMP_DEB
+                "$ESCALATION_TOOL" rm $TEMP_DEB # Removes temp deb file
+                ;;
+            pacman)
+                "$AUR_HELPER" -S --needed --noconfirm cursor-bin
+                ;;
+            dnf)
+                TEMP_RPM="$(mktemp)"
+                wget -O "$TEMP_RPM" "https://api2.cursor.sh/updates/download/golden/linux-x64-rpm/cursor/1.7"
+
+                "$ESCALATION_TOOL" "$PACKAGER" install -y $TEMP_RPM
+                "$ESCALATION_TOOL" rm $TEMP_RPM # Removes temp rpm file
+                ;;
+            *)
+                printf "%b\n" "${RED}Unsupported package manager: ""$PACKAGER""${RC}"
+                exit 1
+                ;;
+        esac
+    else
+        printf "%b\n" "${GREEN}Cursor desktop is already installed.${RC}"
+    fi
+}
+
+# TODO: Maybe find a way to always get the latest version of Cursor for ubuntu/debian
+checkEnv
+checkEscalationTool
+checkAURHelper
+installCursor
