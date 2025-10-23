7ae4ca4652f162e57dd837fd439c3338d4d9f4d8
floofywoofles <k.knapp.jr4@protonmail.com>
2025-10-23 16:09:52 -0400

    apt-search was causing issues for installing, removed it entired. Same with dnf search

--- core/tabs/applications-setup/developer-tools/cursor.sh
+++ core/tabs/applications-setup/developer-tools/cursor.sh
@@ -7,37 +7,31 @@ installCursor() {
         printf "%b\n" "${YELLOW}Installing Cursor...${RC}"
         case "$PACKAGER" in
             apt-get|nala)
-                if apt-cache search "^${PACKAGE_NAME}$" | grep -q "^${PACKAGE_NAME} -"; then
-                    TEMP_DEB="cursor.deb"
+                TEMP_DEB="cursor.deb"
 
-                    curl -sSLo "$TEMP_DEB" 'https://api2.cursor.sh/updates/download/golden/linux-x64-deb/cursor/latest'
+                curl -sSLo "$TEMP_DEB" 'https://api2.cursor.sh/updates/download/golden/linux-x64-deb/cursor/latest'
 
-                    "$ESCALATION_TOOL" "$PACKAGER" update
-                    "$ESCALATION_TOOL" "$PACKAGER" install -y "$TEMP_DEB"
-                    rm "$TEMP_DEB"
-                else
-                    "$ESCALATION_TOOL" "$PACKAGER" install -y cursor
-                fi
+                "$ESCALATION_TOOL" "$PACKAGER" update
+                "$ESCALATION_TOOL" "$PACKAGER" install -y "$TEMP_DEB"
+                rm "$TEMP_DEB"
+                
                 ;;
             pacman)
                 "$AUR_HELPER" -S --needed --noconfirm cursor-bin
                 ;;
             dnf)
-                if dnf list available "cursor" > /dev/null; then
-                    "$ESCALATION_TOOL" "$PACKAGER" install -y cursor
-                else
-                    TEMP_RPM="cursor.rpm"
-                    curl -sSLo "$TEMP_RPM" "https://api2.cursor.sh/updates/download/golden/linux-x64-rpm/cursor/latest"
+                TEMP_RPM="cursor.rpm"
+                curl -sSLo "$TEMP_RPM" "https://api2.cursor.sh/updates/download/golden/linux-x64-rpm/cursor/latest"
 
-                    "$ESCALATION_TOOL" "$PACKAGER" install -y "$TEMP_RPM"
-                    rm "$TEMP_RPM"
-                fi
+                "$ESCALATION_TOOL" "$PACKAGER" install -y "$TEMP_RPM"
+                rm "$TEMP_RPM"
                 ;;
             *)
                 printf "%b\n" "${RED}Unsupported package manager: ${PACKAGER}${RC}"
                 exit 1
                 ;;
         esac
+        printf "%b\n" "${GREEN}Cursor installed successfully.${RC}"
     else
         printf "%b\n" "${GREEN}Cursor is already installed.${RC}"
     fi
