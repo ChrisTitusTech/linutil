db2019e31f67182b4a08feb21fcb9ada127173be
Chris Titus <contact@christitus.com>
2025-12-12 09:45:57 -0600

    add more gaming packages needed for blizzard games

--- core/tabs/system-setup/gaming-setup.sh
+++ core/tabs/system-setup/gaming-setup.sh
@@ -28,12 +28,15 @@ installDepend() {
                     printf "%b\n" "${GREEN}Multilib is already enabled.${RC}"
                 fi
             fi
-            DISTRO_DEPS="gnutls lib32-gnutls base-devel gtk3 lib32-gtk3 python-google-auth python-protobuf libpulse \
-		lib32-libpulse alsa-lib lib32-alsa-lib \
-                alsa-utils alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib giflib lib32-giflib libpng lib32-libpng \
-                libldap lib32-libldap openal lib32-openal libxcomposite lib32-libxcomposite libxinerama lib32-libxinerama \
-                ncurses lib32-ncurses vulkan-icd-loader lib32-vulkan-icd-loader ocl-icd lib32-ocl-icd libva lib32-libva \
-                gst-plugins-base-libs lib32-gst-plugins-base-libs sdl2 lib32-sdl2 v4l-utils lib32-v4l-utils sqlite lib32-sqlite"
+            DISTRO_DEPS="gnutls lib32-gnutls base-devel gtk3 lib32-gtk3 python-google-auth python-protobuf \
+                libpulse lib32-libpulse alsa-lib lib32-alsa-lib alsa-utils alsa-plugins lib32-alsa-plugins \
+                giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap openal lib32-openal \
+                libxcomposite lib32-libxcomposite libxinerama lib32-libxinerama libgcrypt lib32-libgcrypt \
+                libgpg-error lib32-libgpg-error ncurses lib32-ncurses mpg123 lib32-mpg123 \
+                libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libva lib32-libva \
+                gst-plugins-base-libs lib32-gst-plugins-base-libs sdl2 lib32-sdl2 v4l-utils lib32-v4l-utils \
+                vulkan-icd-loader lib32-vulkan-icd-loader ocl-icd lib32-ocl-icd opencl-icd-loader lib32-opencl-icd-loader \
+                libxslt lib32-libxslt cups samba lib32-mesa vulkan-radeon lib32-vulkan-radeon"
 
             $AUR_HELPER -S --needed --noconfirm $DEPENDENCIES $DISTRO_DEPS
             ;;
