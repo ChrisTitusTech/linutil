6687c826e0e722ada2812991b58e4b9dc9202c59
Ivan Lepekha <57459428+FluffyPunk@users.noreply.github.com>
2026-07-03 19:22:03 +0300

    ... I have no need to be asked twice
    Comment out the setupRofiConfig function to prevent execution.

--- core/tabs/applications-setup/rofi-setup.sh
+++ core/tabs/applications-setup/rofi-setup.sh
@@ -24,22 +24,6 @@ installRofi() {
     fi
 }
 
-setupRofiConfig() {
-    printf "%b\n" "${YELLOW}Copying Rofi configuration files...${RC}"
-    if [ -d "$HOME/.config/rofi" ] && [ ! -d "$HOME/.config/rofi-bak" ]; then
-        cp -r "$HOME/.config/rofi" "$HOME/.config/rofi-bak"
-    fi
-    mkdir -p "$HOME/.config/rofi"
-    curl -sSLo "$HOME/.config/rofi/powermenu.sh" https://github.com/ChrisTitusTech/dwm-titus/raw/main/config/rofi/powermenu.sh
-    chmod +x "$HOME/.config/rofi/powermenu.sh"
-    curl -sSLo "$HOME/.config/rofi/config.rasi" https://github.com/ChrisTitusTech/dwm-titus/raw/main/config/rofi/config.rasi
-    mkdir -p "$HOME/.config/rofi/themes"
-    curl -sSLo "$HOME/.config/rofi/themes/nord.rasi" https://github.com/ChrisTitusTech/dwm-titus/raw/main/config/rofi/themes/nord.rasi
-    curl -sSLo "$HOME/.config/rofi/themes/sidetab-nord.rasi" https://github.com/ChrisTitusTech/dwm-titus/raw/main/config/rofi/themes/sidetab-nord.rasi
-    curl -sSLo "$HOME/.config/rofi/themes/powermenu.rasi" https://github.com/ChrisTitusTech/dwm-titus/raw/main/config/rofi/themes/powermenu.rasi
-}
 
 checkEnv
 installRofi
-# Commented out in case Chris remembers that he deleted Rofi config and will recover that
-#setupRofiConfig
