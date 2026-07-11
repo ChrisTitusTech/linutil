777887d262d121deb28763d37f6224d45dd1dfc8
UmutKzl <kzzzl@proton.me>
2026-07-11 12:53:29 +0300

    feat(browsers): add helium-browser

--- core/tabs/applications-setup/browsers/helium-browser.sh
+++ core/tabs/applications-setup/browsers/helium-browser.sh
@@ -27,9 +27,41 @@ installHeliumBrowser() {
     else
         printf "%b\n" "${GREEN}Helium Browser is already installed.${RC}"
     fi
+uninstallHeliumBrowser() {
+  if command_exists Helium && commands_exists helium; then
+    printf "%b\n" "${YELLOW}Removing Helium Browser...${RC}"
+    case "$PACKAGER" in
+    pacman)
+      "$ESCALATION_TOOL" "$PACKAGER" -R --noconfirm --needed helium-browser-bin
+      ;;
+    dnf)
+      "$ESCALATION_TOOL" "$PACKAGER" uninstall helium-bin
+      "$ESCALATION_TOOL" "$PACKAGER" copr disable imput/helium
+      ;;
+    nala | apt-get)
+      [[ -f "/usr/share/keyrings/helium.gpg" ]] && rm "/usr/share/keyrings/helium.gpg"
+      [[ -f "/etc/apt/sources.list.d/helium.list" ]] && rm "/etc/apt/sources.list.d/helium.list"
+      ;;
+    esac
+  else
+    printf "%b\n" "${GREEN}Helium Browser is not installed.${RC}"
+  fi
+}
+
+main() {
+  printf "%b\n" "${YELLOW}Do you want to Install or Uninstall Helium Browser?${RC}"
+  printf "%b\n" "1. ${YELLOW}Install Helium Browser${RC}"
+  printf "%b\n" "2. ${YELLOW}Uninstall Helium Browser${RC}"
+  printf "%b" "Enter your choice [1-2]: "
+  read -r CHOICE
+  case "$CHOICE" in
+  1) installHeliumBrowser ;;
+  2) uninstallHeliumBrowser ;;
+  *) printf "%b\n" "${RED}Invalid choice.${RC}" && exit 1 ;;
+  esac
 }
 
 checkEnv
 checkEscalationTool
 checkAURHelper
-installHeliumBrowser
+main
