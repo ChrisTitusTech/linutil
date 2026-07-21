510996c8b78251066205790393a3f31f2c334139
Nick A <60294463+technicks89@users.noreply.github.com>
2026-07-20 20:26:10 -0400

    Updated to use FEDORA_VERSION variable where applicable

--- core/tabs/system-setup/fedora/rpm-fusion-setup.sh
+++ core/tabs/system-setup/fedora/rpm-fusion-setup.sh
@@ -12,8 +12,8 @@ installRPMFusion() {
                 printf "%b\n" "${YELLOW}Installing RPM Fusion...${RC}"
 
                 "$ESCALATION_TOOL" "$PACKAGER" install -y \
-                    "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
-                    "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
+                    "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$FEDORA_VERSION).noarch.rpm" \
+                    "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$FEDORA_VERSION.noarch.rpm"
 
                 if [ "$FEDORA_VERSION" -ge 41 ]; then
                     "$ESCALATION_TOOL" "$PACKAGER" config-manager setopt fedora-cisco-openh264.enabled=1
